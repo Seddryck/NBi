8d3b48b0e11345c4894df21f4236b247a8ac53cc
Cédric L. Charlier <cedric.charlier@yucom.be>
2012-01-04 01:13:44 +0100

    Added first support for Xml definition of TestCase

---
+++ NBi.Testing/Xml/PlayConstraintTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using NBi.NUnit;
+using NBi.Xml;
+using NUnit.Framework;
+
+namespace NBi.Testing.Xml
+{
+    [TestFixture]
+    public class PlayConstraintTest
+    {
+        protected string _connectionString;
+
+        #region Setup & Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            _connectionString = "Data Source=.;Initial Catalog=NBi.Testing;Integrated Security=True";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        #endregion
+        
+        [Test]
+        public void TestCase_Play_Success()
+        {
+            var constraint = new QueryParserConstraint(_connectionString);
+            var testCase = new TestCaseXml() { Sql = "SELECT * FROM Product;" };
+
+            testCase.Play(constraint);
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_Play_Success()
+        {
+            var t = new TestXml()
+            {
+                Constraints = new List<AbstractConstraintXml>() { new QueryParserXml() { ConnectionString = _connectionString } },
+                TestCases = new List<TestCaseXml>() { new TestCaseXml() { Sql = "SELECT * FROM Product;" } }
+            };
+
+            t.Play();
+
+            Assert.Pass();
+        }
+    }
+}
---
+++ NBi.Xml/TestXml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using NUnit.Framework.Constraints;
+
+namespace NBi.Xml
+{
+    public class TestXml
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+        
+        [XmlAttribute("uid")]
+        public string UniqueIdentifier { get; set; }
+
+        [XmlElement(Type = typeof(QueryParserXml), ElementName="QueryParser"),
+        XmlElement(Type = typeof(QueryPerformanceXml), ElementName = "QueryPerformance")]
+        public List<AbstractConstraintXml> Constraints;
+
+        [XmlElement("TestCase")]
+        public List<TestCaseXml> TestCases;
+
+        public IList<Constraint> Instantiate()
+        {
+            var list = new List<Constraint>();
+            foreach (var constraint in Constraints)
+            {
+                var c = constraint.Define();
+                list.Add(c);
+            }
+            return list;
+        }
+
+        public void Play()
+        {
+            var ctrs = Instantiate();
+            foreach (var c in ctrs)
+            {
+                foreach (var tc in TestCases)
+                {
+                    tc.Play(c);
+                }
+            }
+        }
+    }
+}

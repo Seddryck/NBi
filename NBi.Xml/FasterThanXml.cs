7277776d3db5bf9f3ce0e48683a4a86ec8ab8b54
Seddryck <Seddryck@localhost>
2012-01-05 13:59:22 +0100

    New constraints files

---
+++ NBi.Xml/FasterThanXml.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+using NBi.NUnit;
+using NUnit.Framework.Constraints;
+
+namespace NBi.Xml
+{
+    public class FasterThanXml : AbstractConstraintXml
+    {
+        [XmlAttribute("connectionString")]
+        public string ConnectionString { get; set; }
+
+        [XmlAttribute("maxTimeMilliSeconds")]
+        public int MaxTimeMilliSeconds { get; set; }
+
+        public override Constraint Define()
+        {
+            var ctr = new FasterThanConstraint(ConnectionString, MaxTimeMilliSeconds);
+            return ctr;
+        }
+    }
+}

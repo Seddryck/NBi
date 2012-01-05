ffc0c83a477c7b3ad807db4da65509a056364cde
Cédric L. Charlier <cedric.charlier@yucom.be>
2012-01-05 13:59:22 +0100

    New constraints files

---
+++ NBi.Testing/NUnit/Database/FasterThanConstraintTest.cs
@@ -0,0 +1,80 @@
+﻿using Moq;
+using NBi.Core;
+using NBi.Core.Database;
+using NBi.NUnit;
+using NUnit.Framework;
+
+namespace NBi.Testing.NUnit.Database
+{
+    [TestFixture]
+    public class FasterThanConstraintTest
+    {
+
+        protected string _connectionString;
+
+        #region Setup & Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+            //If available use the user file
+            if (System.IO.File.Exists("ConnectionString.user.config"))
+            {
+                _connectionString = System.IO.File.ReadAllText("ConnectionString.user.config");
+            }
+            else if (System.IO.File.Exists("ConnectionString.config"))
+            {
+                _connectionString = System.IO.File.ReadAllText("ConnectionString.config");
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        #endregion
+
+        [Test]
+        public void QueryPerformanceRealImplementation_QueryPerformanceConstraint_Success()
+        {
+            var sql = "SELECT * FROM Product;";
+
+            //Method under test
+            Assert.That(sql, new FasterThanConstraint(_connectionString, 5000));
+
+            //Test conclusion            
+            Assert.Pass();
+        }
+
+        [Test]
+        public void QueryPerformanceRealImplementation_IsFasterThan_Success()
+        {
+            var sql = "SELECT * FROM Product;";
+
+            Assert.That(sql, OnDataSource.Localized(_connectionString).Is.FasterThan(5000));
+            
+            Assert.Pass();
+        }
+
+        [Test]
+        public void QueryPerformanceMock_IsFasterThan_CalledOnce()
+        {
+            var sql = "SELECT * FROM Product;";
+            var mock = new Mock<IQueryPerformance>();
+
+            mock.Setup(engine => engine.Validate(sql))
+                .Returns(Result.Success());
+            IQueryPerformance qp = mock.Object;
+
+            var qpc = new FasterThanConstraint(qp);
+
+            //Method under test
+            Assert.That(sql, qpc);
+
+            //Test conclusion            
+            mock.Verify(engine => engine.Validate(sql), Times.AtMostOnce());
+        }
+
+    }
+}

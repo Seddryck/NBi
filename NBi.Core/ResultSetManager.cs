e3ffa6b5eba21388581942bff2cc7aa3f4eba505
Cédric L. Charlier <cedric.charlier@yucom.be>
2012-04-27 01:00:42 +0200

    Review of Tests

---
+++ NBi.Core/ResultSetManager.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using NBi.Core.Analysis.Query;
+
+namespace NBi.Core
+{
+    public class ResultSetManager
+    {
+        protected IResultSetWriter _resultSetWriter;
+        protected IQueryExecutor _queryExecutor;
+
+        protected internal ResultSetManager(IResultSetWriter resultSetWriter, IQueryExecutor queryExecutor)
+        {
+            _resultSetWriter = resultSetWriter;
+            _queryExecutor= queryExecutor;
+        }
+
+        public static ResultSetManager Instantiate(string resultSetDirectory, string connectionString)
+        {
+            return new ResultSetManager(
+                new ResultSetCsvWriter(resultSetDirectory),
+                new OleDbExecutor(connectionString));
+        }
+        
+        public void CreateResultSet(string queriesDirectory)
+        {
+            var queryFiles = System.IO.Directory.EnumerateFiles(queriesDirectory);
+
+            if(!Directory.Exists(_resultSetWriter.PersistencePath))
+                Directory.CreateDirectory(_resultSetWriter.PersistencePath);
+
+            foreach (var queryFile in queryFiles)
+            {
+                string query;
+                using (StreamReader infile = new StreamReader(Path.Combine(queriesDirectory, queryFile)))
+                {
+                    query=infile.ReadToEnd();
+                }
+
+                var ds = _queryExecutor.Execute(query);
+
+                var resultFile = Path.GetFileName(Path.ChangeExtension(queryFile,"csv"));
+                _resultSetWriter.Write(resultFile, ds);
+            }
+        }
+    }
+}

93f804372813a81f854f8feca17d9d87ddeb9fb3
Seddryck <Seddryck@localhost>
2012-08-09 21:43:56 +0200

    Renamed Acceptance by Integration in Testing dll

---
+++ NBi.Testing/Integration/Core/Analysis/Metadata/MetadataAdomdExtractorTest.cs
@@ -0,0 +1,90 @@
+﻿using NBi.Core.Analysis;
+using NBi.Core.Analysis.Metadata;
+using NUnit.Framework;
+
+namespace NBi.Testing.Integration.Core.Analysis.Metadata
+{
+    [TestFixture]
+    public class MetadataAdomdExtractorTest
+    {
+        [Test]
+        public void GetMetadata_ExistingCube_ListOfMetadata()
+        {
+            var me = new MetadataAdomdExtractor(ConnectionStringReader.GetAdomd());
+            
+            var metadata = me.GetFullMetadata();
+
+            Assert.That(metadata.Perspectives["Adventure Works"].Dimensions.ContainsKey("[Date]"));
+            Assert.That(!metadata.Perspectives["Adventure Works"].Dimensions.ContainsKey("[Measures]"));
+
+            Assert.That(metadata.Perspectives["Adventure Works"].Dimensions["[Date]"].Hierarchies.ContainsKey("[Date].[Calendar]"));
+
+            Assert.That(metadata.Perspectives["Adventure Works"].Dimensions["[Date]"].Hierarchies["[Date].[Calendar]"].Levels.ContainsKey("[Date].[Calendar].[Month]"));
+
+            Assert.That(metadata.Perspectives["Adventure Works"]
+                .Dimensions["[Date]"]
+                .Hierarchies["[Date].[Calendar]"]
+                .Levels["[Date].[Calendar].[Month]"]
+                .Properties.ContainsKey("[Date].[Calendar].[Month].[Calendar Quarter]"));
+
+            Assert.That(metadata.Perspectives["Adventure Works"].MeasureGroups.ContainsKey("Financial Reporting"));
+            Assert.That(!metadata.Perspectives["Adventure Works"].MeasureGroups.ContainsKey("[Date]"));
+
+            Assert.That(metadata.Perspectives["Adventure Works"].MeasureGroups["Financial Reporting"].LinkedDimensions.ContainsKey("[Date]"));
+            Assert.That(!metadata.Perspectives["Adventure Works"].MeasureGroups["Financial Reporting"].LinkedDimensions.ContainsKey("[Measures]"));
+
+            Assert.That(metadata.Perspectives["Adventure Works"].MeasureGroups["Financial Reporting"].Measures.ContainsKey("[Measures].[Amount]"));
+        }
+
+        public void GetChildStructure_DateDimensionWithHeighTeenHierarchies_ListStructureContainingHeighTeenElements()
+        {
+            var me = new MetadataAdomdExtractor(ConnectionStringReader.GetAdomd());
+            var disco = new DiscoverCommand(ConnectionStringReader.GetAdomd());
+            disco.Perspective = "Adventure Works";
+            disco.Path = "[Date]";
+
+            var structs = me.GetPartialMetadata(disco);
+
+            Assert.That(structs, Has.Count.EqualTo(18));
+        }
+
+        public void GetChildStructure_CalendarHierarchyWithSixLevels_ListStructureContainingSixElements()
+        {
+            var me = new MetadataAdomdExtractor(ConnectionStringReader.GetAdomd());
+            var disco = new DiscoverCommand(ConnectionStringReader.GetAdomd());
+            disco.Perspective = "Adventure Works";
+            disco.Path = "[Date].[Calendar]";
+
+            var structs = me.GetPartialMetadata(disco);
+
+            Assert.That(structs, Has.Count.EqualTo(6));
+        }
+
+        public void GetChildStructure_MonthLevelWithTwoProperties_ListStructureContainingTwoElements()
+        {
+            var me = new MetadataAdomdExtractor(ConnectionStringReader.GetAdomd());
+            var disco = new DiscoverCommand(ConnectionStringReader.GetAdomd());
+            disco.Perspective = "Adventure Works";
+            disco.Path = "[Date].[Calendar].[Month]";
+
+            var structs = me.GetPartialMetadata(disco);
+
+            Assert.That(structs, Has.Count.EqualTo(2));
+        }
+
+        public void GetMeasuresOfFolder_PerspectiveAdventureWorks_OneElement()
+        {
+            var me = new MetadataAdomdExtractor(ConnectionStringReader.GetAdomd());
+            var disco = new DiscoverCommand(ConnectionStringReader.GetAdomd());
+            disco.Perspective = "Finance";
+            disco.Path = "[Measures].[Financial Reporting]"; //Financial Reporting is the folder's name!
+
+            var structs = me.GetPartialMetadata(disco);
+
+            Assert.That(structs, Has.Count.EqualTo(1));
+        }
+
+        
+
+    }
+}
---
+++ NBi.Testing/Integration/Core/Query/QueryAdomdEngineTest.cs
@@ -0,0 +1,70 @@
+﻿using NBi.Core.Query;
+using NUnit.Framework;
+using Microsoft.AnalysisServices.AdomdClient;
+
+namespace NBi.Testing.Integration.Core.Query
+{
+    [TestFixture]
+    public class QueryAdomdEngineTest
+    {
+        [Test]
+        public void Execute_ValidMdx_GetResult()
+        {
+
+            var query = "SELECT  [Measures].[Amount] ON 0, [Date].[Calendar].[Calendar Year] ON 1 FROM [Adventure Works]";
+            var cmd = new AdomdCommand(query, new AdomdConnection(ConnectionStringReader.GetAdomd()));
+
+            var qe = new QueryAdomdEngine(cmd);
+            var ds = qe.Execute();
+
+            Assert.IsInstanceOf<string>(ds.Tables[0].Rows[0][0]);
+            Assert.AreEqual((string)ds.Tables[0].Rows[0][0], "CY 2001");
+            Assert.AreEqual((string)ds.Tables[0].Rows[1][0], "CY 2002");
+            Assert.IsInstanceOf<double>(ds.Tables[0].Rows[1][1]);
+        }
+
+        [Test]
+        public void Execute_ValidMdxWithNull_GetResult()
+        {
+
+            var query = "SELECT  [Measures].[Amount] ON 0, [Date].[Calendar].[Calendar Year].&[2006] ON 1 FROM [Adventure Works]";
+            var cmd = new AdomdCommand(query, new AdomdConnection(ConnectionStringReader.GetAdomd()));
+
+            var qe = new QueryAdomdEngine(cmd);
+            var ds = qe.Execute();
+
+            Assert.IsInstanceOf<string>(ds.Tables[0].Rows[0][0]);
+            Assert.AreEqual((string)ds.Tables[0].Rows[0][0], "CY 2006");
+            
+            Assert.IsInstanceOf<System.DBNull>(ds.Tables[0].Rows[0][1]);
+            Assert.That(ds.Tables[0].Rows[0].IsNull(1), Is.True);
+        }
+
+        [Test]
+        public void Parse_ValidMdx_Success()
+        {
+
+            var query = "SELECT  [Measures].[Amount] ON 0, [Date].[Calendar].[Calendar Year].&[2006] ON 1 FROM [Adventure Works]";
+            var cmd = new AdomdCommand(query, new AdomdConnection(ConnectionStringReader.GetAdomd()));
+
+            var qe = new QueryAdomdEngine(cmd);
+            var pr = qe.Parse();
+
+            Assert.That(pr.IsSuccesful, Is.True);
+        }
+
+        [Test]
+        public void Parse_NotValidMdx_Failed()
+        {
+
+            var query = "SELECT  [Measures].[NonEXisting] ON 0, [Date].[Calendar].[Calendar Year].&[2006] ON 1 FROM [Adventure Works]";
+            var cmd = new AdomdCommand(query, new AdomdConnection(ConnectionStringReader.GetAdomd()));
+
+            var qe = new QueryAdomdEngine(cmd);
+            var pr = qe.Parse();
+
+            Assert.That(pr.IsSuccesful, Is.False);
+        }
+
+    }
+}

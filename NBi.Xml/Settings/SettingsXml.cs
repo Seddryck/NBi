f5f06badcdeef103a84b17f3b339c807ecd5b23d
Seddryck <Seddryck@localhost>
2012-08-16 18:29:28 +0200

    Two files forgotten when Added implementation of Settings and Default -ConnectionString- in Xml

---
+++ NBi.Xml/Settings/SettingsXml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.Linq;
+
+namespace NBi.Xml.Settings
+{
+    public class SettingsXml
+    {
+        [XmlElement("default")]
+        public List<DefaultXml> Defaults { get; set; }
+
+        public enum DefaultScope
+        {
+            [XmlEnum(Name = "system-under-test")]
+            SystemUnderTest,
+            [XmlEnum(Name = "assert")]
+            Assert
+        }
+
+        public DefaultXml GetDefault(DefaultScope scope)
+        {
+            return Defaults.SingleOrDefault(d => d.ApplyTo == scope);
+        }
+
+    }
+}

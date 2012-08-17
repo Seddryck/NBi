58a77b7c4e8b49df36c23774a032ee4f0833da03
Cédric L. Charlier <cedric.charlier@yucom.be>
2012-08-18 01:12:51 +0200

    Comment the solution for execution of acceptance Tests Cases

--- NBi.Testing/Acceptance/TestSuiteRunner.cs
+++ NBi.Testing/Acceptance/TestSuiteRunner.cs
@@ -6,19 +6,32 @@ namespace NBi.Testing.Acceptance
     [TestFixture]
     public class TestSuiteRunner
     {
+        /// <summary>
+        /// Runner to load and execute the Acceptance Tests with the code as close as possible to the true Runtime
+        /// <remarks>Yes I know it's a bit tricky</remarks>
+        /// </summary>
         [Test]
         protected void Run()
         {
+            //Define the name of itself
             var assemblyName = @"NBi.Testing.dll";
 
+            //Instantiate a SimpleTestRunner
             CoreExtensions.Host.InitializeService();
             SimpleTestRunner runner = new SimpleTestRunner();
+
+            //Define the test package as all the tests of this assembly in the class RuntimeOverrider
+            //The assembly (and so the tests) will be filtered based on TestName
             TestPackage package = new TestPackage( "Test");
-            package.TestName = "NBi.Testing.Acceptance.RuntimeOverrider";
+            package.TestName = "NBi.Testing.Acceptance.RuntimeOverrider"; //Filter
             package.Assemblies.Add(assemblyName);
+
+            //Load the tests from the filtered package (so we don't need to filter again!)
             if( runner.Load(package) )     
             {         
+                //Run all the tests (Have I said I've previsously filtered ? ... No seriously you read this kind of comment?)
                 TestResult result = runner.Run( new NullListener(), TestFilter.Empty, false, LoggingThreshold.Off ); 
+                //Ensure the acceptance test suite is fully positive!
                 Assert.That(result.IsSuccess, Is.True);    
             }
             else

f39c16d7580bb282389cc4e507ed9cbbecb1eefd
Seddryck <Seddryck@localhost>
2012-08-18 01:12:51 +0200

    Comment the solution for execution of acceptance Tests Cases

--- NBi.Testing/Acceptance/RuntimeOverrider.cs
+++ NBi.Testing/Acceptance/RuntimeOverrider.cs
@@ -4,9 +4,17 @@ using NBi.Xml;
 
 namespace NBi.Testing.Acceptance
 {
+    /// <summary>
+    /// This class is the only one in the namespace "NBi.Testing.Acceptance" with a TestFixture.
+    /// NUnit (more specifically the SimpleTestRunner created in method Run of class TestSuiteRunner) 
+    /// will load this class as the entry point for Acceptance Test Suites.
+    /// </summary>
     [TestFixture]
     public class RuntimeOverrider
     {
+        
+        //This class overrides the search for TestSuiteDefinitionFile
+        //The filename is given by the TestCase here under
         public class TestSuiteOverrider : TestSuite
         {
             
@@ -22,7 +30,7 @@ namespace NBi.Testing.Acceptance
             }
         }
         
-
+        //By Acceptance Test Suite (file) create a Test Case
         [Test]
         [TestCase("QueryEqualToCsv.xml")]
         [TestCase("QueryEqualToQuery.xml")]
@@ -31,7 +39,11 @@ namespace NBi.Testing.Acceptance
         {
             var t = new TestSuiteOverrider(filename);
             
+            //First retrieve the NUnit TestCases with base class (NBi.NUnit.Runtime)
+            //These NUnit TestCases are defined in the Test Suite file
             var tests = t.GetTestCases();
+
+            //Execute the NUnit TestCases one by one
             foreach (var testCaseData in tests)
                 t.ExecuteTestCases((TestXml)testCaseData.Arguments[0]);
             

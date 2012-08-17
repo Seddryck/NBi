d0f28e95e5ea15fc4905faecad55090c36e49edd
Cédric L. Charlier <cedric.charlier@yucom.be>
2012-08-18 00:55:33 +0200

    Created Runner for Acceptance Testing

---
+++ NBi.Testing/Acceptance/RuntimeOverrider.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using NBi.NUnit.Runtime;
+using NBi.Xml;
+
+namespace NBi.Testing.Acceptance
+{
+    [TestFixture]
+    public class RuntimeOverrider
+    {
+        public class TestSuiteOverrider : TestSuite
+        {
+            
+            private string _filename;
+            public TestSuiteOverrider(string filename)
+            {
+                _filename = filename;
+            }
+            
+            protected override string GetTestSuiteFileDefinition()
+            {
+                return @"Acceptance\Resources\" + _filename;
+            }
+        }
+        
+
+        [Test]
+        [TestCase("QueryEqualToCsv.xml")]
+        [TestCase("QueryEqualToQuery.xml")]
+        [TestCase("QueryEqualToResultSet.xml")]
+        public void RunTestSuite(string filename)
+        {
+            var t = new TestSuiteOverrider(filename);
+            
+            var tests = t.GetTestCases();
+            foreach (var testCaseData in tests)
+                t.ExecuteTestCases((TestXml)testCaseData.Arguments[0]);
+            
+        }
+    }
+}

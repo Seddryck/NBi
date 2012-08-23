d65b9fb8cfab1dcde65cc2e94b82070adc31ca55
Cédric L. Charlier <cedric.charlier@yucom.be>
2012-08-23 23:54:47 +0200

    Added GlobalAssemblyInfo.cs -oups-

--- NBi.UI/Properties/AssemblyInfo.cs
+++ NBi.UI/Properties/AssemblyInfo.cs
@@ -7,11 +7,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NBi.UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
-[assembly: AssemblyProduct("NBi.UI")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("df4588b9-ce45-4d9e-ab67-2a5738be8d25")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]

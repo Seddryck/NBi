52a5bb659a0b5b421678f74b064dd985147a15ef
Seddryck <Seddryck@localhost>
2012-08-27 22:13:21 +0200

    Introduction of Assembly into Constraints

---
+++ NBi.Testing/Unit/Core/Assemblies/Resource/Klass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Testing.Unit.Core.Assemblies.Resource
+{
+    class Klass
+    {
+        public Klass()
+        {
+
+        }
+
+        public string ExecutePublicString(string paramString)
+        {
+            if (paramString == "MyString")
+                return "Executed";
+            return "Incorrect Parameters";
+        }
+
+        private string ExecutePrivateString(string paramString)
+        {
+            if (paramString == "MyString")
+                return "Executed";
+            return "Incorrect Parameters";
+        }
+
+        public string ExecuteDoubleParam(DateTime paramDateTime, Enumeration paramEnum)
+        {
+            if (paramDateTime == new DateTime(2012,05,07) && paramEnum == Enumeration.Beta)
+                return "Executed";
+            return "Incorrect Parameters";
+        }
+    }
+}
---
+++ NBi.Xml/Systems/ParameterXml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Xml.Serialization;
+using NBi.Core;
+
+namespace NBi.Xml.Systems
+{
+    public class ParameterXml
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlText]
+        public object Value { get; set; }
+    }
+}

45c4e5d8c6a35a04b2ab6c59795e189ad706781f
Cédric L. Charlier <cedric.charlier@yucom.be>
2012-09-03 23:33:56 +0200

    Add InternalVisibleTo attribute

--- NBi.NUnit.Runtime/Properties/AssemblyInfo.cs
+++ NBi.NUnit.Runtime/Properties/AssemblyInfo.cs
@@ -9,6 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 
+//Reference the testing class to ensure access to internal members
+[assembly: InternalsVisibleTo("NBi.Testing")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.

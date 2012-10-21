085dcbf892fc10ac2ef6dc8efba8d30d391ec3dc
Seddryck <Seddryck@localhost>
2012-10-21 22:36:54 +0200

    DisplayFolder from Exists and Contains to Measure Measure-Group is OPtional in Xml

--- NBi.Xml/Systems/Structure/MeasureXml.cs
+++ NBi.Xml/Systems/Structure/MeasureXml.cs
@@ -12,13 +12,53 @@ namespace NBi.Xml.Systems.Structure
             return true;
         }
 
+        protected string measureGroup;
         [XmlAttribute("measure-group")]
-        public string MeasureGroup { get; set; }
+        public string MeasureGroup
+        {
+            get
+            { return measureGroup; }
+
+            set
+            {
+                measureGroup = value;
+                Specification.IsMeasureGroupSpecified = true;
+            }
+        }
+
+        protected string displayFolder;
+        [XmlAttribute("display-folder")]
+        public string DisplayFolder
+        {
+            get
+            { return displayFolder; }
+
+            set
+            {
+                displayFolder = value;
+                Specification.IsDisplayFolderSpecified = true;
+            }
+        }
+
+        public MeasureXml()
+        {
+            Specification = new SpecificationMeasure();
+        }
 
         public override object Instantiate()
         {
             var cmd = DiscoveryFactory.BuildForMeasureGroup(ConnectionString ?? Default.ConnectionString, Perspective, MeasureGroup);
             return cmd;
         }
+
+        [XmlIgnore()]
+        public SpecificationMeasure Specification { get; protected set; }
+
+        public class SpecificationMeasure
+        {
+            public bool IsDisplayFolderSpecified { get; internal set; }
+            public bool IsMeasureGroupSpecified { get; internal set; }
+        }
+
     }
 }

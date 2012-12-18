a0d8b7332fea521552ba3212f2d623da067d140a
Cédric L. Charlier <cedric.charlier@yucom.be>
2012-12-18 15:22:10 +0100

    Added Assert.Pass() to specify that the test is not missing

--- NBi.Testing/Unit/Core/ResultSet/LineFormatterTest.cs
+++ NBi.Testing/Unit/Core/ResultSet/LineFormatterTest.cs
@@ -9,26 +9,27 @@ using NUnit.Framework.Constraints;
 
 namespace NBi.Testing.Unit.Core.ResultSet
 {
-  [TestFixture]
-  public class LineFormatterTest
-  {
-    [Test]
-    public void GetText_ShortColumn_NoException()
+    [TestFixture]
+    public class LineFormatterTest
     {
-      // Design Dummy Column
-      DataColumn col = new DataColumn("DummyColumn");
-      col.ExtendedProperties.Add("NBi::Role", ColumnRole.Key);
-      col.ExtendedProperties.Add("NBi::Type", ColumnType.Numeric);
-      col.ExtendedProperties.Add("NBi::Tolerance", (decimal)0);
-      
-      // Design dummy table
-      DataTable table = new DataTable();
-      table.Columns.Add(col);
+        [Test]
+        public void GetText_ShortColumn_NoException()
+        {
+            // Design Dummy Column
+            DataColumn col = new DataColumn("DummyColumn");
+            col.ExtendedProperties.Add("NBi::Role", ColumnRole.Key);
+            col.ExtendedProperties.Add("NBi::Type", ColumnType.Numeric);
+            col.ExtendedProperties.Add("NBi::Tolerance", (decimal)0);
 
-      ICellFormatter cf = LineFormatter.BuildHeader(table, 0);
-      
-      // This must not throw an exception when the header is bigger that requested size
-      cf.GetText(4);
+            // Design dummy table
+            DataTable table = new DataTable();
+            table.Columns.Add(col);
+
+            ICellFormatter cf = LineFormatter.BuildHeader(table, 0);
+
+            // This must not throw an exception when the header is bigger that requested size
+            cf.GetText(4);
+            Assert.Pass();
+        }
     }
-  }
 }

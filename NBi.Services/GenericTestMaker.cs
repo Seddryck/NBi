f0f6b2ced309c49564fa5a15b1989406c59e71e7
C?dric Charlier <cedric.charlier@yucom.be>
2012-12-19 23:44:00 +0100

    First try for the services UI

---
+++ NBi.Services/GenericTestMaker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using NBi.Xml;
+
+namespace NBi.Service
+{
+    public class GenericTestMaker
+    {
+        public string TemplateXml { get; private set; }
+
+        public GenericTestMaker(string templateXml)
+        {
+            TemplateXml = templateXml;
+        }
+
+        public IEnumerable<TestXml> Build(IEnumerable<IList<string>> rows)
+        {
+            var tests = new List<TestXml>();
+
+            foreach (var row in rows)
+	        {
+                var str = string.Format(TemplateXml, row.ToArray());
+                var test = XmlDeserializeFromString <TestStandaloneXml>(str);
+                tests.Add(test);
+	        }
+
+            return tests;
+        }
+
+        protected internal T XmlDeserializeFromString<T>(string objectData)
+        {
+            return (T)XmlDeserializeFromString(objectData, typeof(T));
+        }
+
+        protected object XmlDeserializeFromString(string objectData, Type type)
+        {
+            var serializer = new XmlSerializer(type);
+            object result;
+
+            using (TextReader reader = new StringReader(objectData))
+            {
+                result = serializer.Deserialize(reader);
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
---
+++ NBi.Testing/Unit/Service/GenericTestMakerTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NBi.Service;
+using NBi.Xml.Constraints;
+using NBi.Xml.Items;
+using NBi.Xml.Systems;
+using NUnit.Framework;
+
+namespace NBi.Testing.Unit.Service
+{
+    [TestFixture]
+    public class GenericTestMakerTest
+    {
+
+        #region SetUp & TearDown
+        //Called only at instance creation
+        [TestFixtureSetUp]
+        public void SetupMethods()
+        {
+
+        }
+
+        //Called only at instance destruction
+        [TestFixtureTearDown]
+        public void TearDownMethods()
+        {
+        }
+
+        //Called before each test
+        [SetUp]
+        public void SetupTest()
+        {
+        }
+
+        //Called after each test
+        [TearDown]
+        public void TearDownTest()
+        {
+        }
+        #endregion
+
+        [Test]
+        public void Build_ExistsTemplateForOneDimension_CorrectTest()
+        {
+            var template =
+                "<test name=\"In dimension '{2}', hierarchy '{1}', a level '{0}' is existing\">" +
+                "    <system-under-test>" +
+                "        <structure>" +
+                "            <level caption=\"{0}\" dimension=\"{2}\" hierarchy=\"{1}\" perspective=\"{3}\"/>" +
+                "        </structure>" +
+                "    </system-under-test>" +
+                "    <assert>" +
+                "        <exists/>" +
+                "    </assert>" +
+                "</test>";
+
+            var info = new List<string[]>()
+                {
+                    new string[] 
+                    {
+                        "myLevel",
+                        "myHierarchy",
+                        "myDimension",
+                        "myPerspective"
+                    }
+                };
+
+            var gtm = new GenericTestMaker(template);
+
+            var results = gtm.Build(info);
+
+            Assert.That(results, Has.Count.EqualTo(1));
+
+            var result = results.ElementAt(0);
+            Assert.That(result.Name, Is.EqualTo("In dimension 'myDimension', hierarchy 'myHierarchy', a level 'myLevel' is existing"));
+            Assert.That(result.Systems[0], Is.TypeOf<StructureXml>());
+            Assert.That(((StructureXml)result.Systems[0]).Item, Is.TypeOf<LevelXml>());
+            Assert.That(result.Constraints[0], Is.TypeOf<ExistsXml>());
+
+        }
+
+        [Test]
+        public void Build_ExistsTemplateForTwoDimensions_TwoTestsReturned()
+        {
+            var template =
+                "<test name=\"In dimension '{2}', hierarchy '{1}', a level '{0}' is existing\">" +
+                "    <system-under-test>" +
+                "        <structure>" +
+                "            <level caption=\"{0}\" dimension=\"{2}\" hierarchy=\"{1}\" perspective=\"{3}\"/>" +
+                "        </structure>" +
+                "    </system-under-test>" +
+                "    <assert>" +
+                "        <exists/>" +
+                "    </assert>" +
+                "</test>";
+
+            var info = new List<string[]>()
+                {
+                    new string[] 
+                    {
+                        "myLevel",
+                        "myHierarchy",
+                        "myDimension",
+                        "myPerspective"
+                    },
+                    new string[] 
+                    {
+                        "myOtherLevel",
+                        "myHierarchy",
+                        "myDimension",
+                        "myPerspective"
+                    }
+                };
+
+            var gtm = new GenericTestMaker(template);
+
+            var results = gtm.Build(info);
+
+            Assert.That(results, Has.Count.EqualTo(2));
+        }
+    }
+}

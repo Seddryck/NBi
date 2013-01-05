6edb6050c1ee63cb9fa071d1f9c23e4f1549b709
C?dric Charlier <cedric.charlier@yucom.be>
2013-01-05 16:52:20 +0100

    Add the connection string to the error message when the connection string was in cause.

---
+++ NBi.Core/NBiException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Core
+{
+    public class NBiException : Exception
+    {
+        public NBiException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}

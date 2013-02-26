a37567ce005f8e80f16c67ced7e06b9e44fc6f26
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-02-26 23:40:49 +0100

    Document the NBI.Core.Query namespace and change some public into internal or remove static keywords

--- NBi.Core/Query/ListBuilder.cs
+++ NBi.Core/Query/ListBuilder.cs
@@ -5,9 +5,17 @@ using System.Linq;
 
 namespace NBi.Core.Query
 {
+    /// <summary>
+    /// This class build a list of string froma query taking the first cell of each row
+    /// </summary>
     public class ListBuilder
     {
-        public virtual IList<string> Build(IDbCommand cmd)
+        /// <summary>
+        /// Execute the command and take the first cell of each row to build an Enumerable of string
+        /// </summary>
+        /// <param name="cmd">A sql or mdx query to execute</param>
+        /// <returns>The first cell of row returned by the query</returns>
+        public virtual IEnumerable<string> Build(IDbCommand cmd)
         {
             var qe = new QueryEngineFactory().GetExecutor(cmd);
             var ds = qe.Execute();

44699cbd3bb2826bc2c4098053782f0cf9084dfc
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-02-26 23:39:49 +0100

    Document the class in the resources folder needed to test assembly item

--- NBi.Testing/Acceptance/Resources/AssemblyClass.cs
+++ NBi.Testing/Acceptance/Resources/AssemblyClass.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace NBi.Testing.Acceptance.Resources
 {
+    /// <summary>
+    /// This class is only used for acceptance testing purpose
+    /// </summary>
     class AssemblyClass
     {
         public AssemblyClass()
@@ -12,6 +15,11 @@ namespace NBi.Testing.Acceptance.Resources
 
         }
 
+        /// <summary>
+        /// Method returning a valid MDX query
+        /// </summary>
+        /// <param name="calendarYear">A year between 2001 and 2006 preceded by 'CY'</param>
+        /// <returns>Valid MDX query</returns>
         public string GetSelectMdx(string calendarYear)
         {
             var mdx = string.Format("SELECT [Measures].[Reseller Order Count] ON 0, [Date].[Calendar Year].[{0}] ON 1 FROM [Adventure Works]", calendarYear);
@@ -24,6 +32,12 @@ namespace NBi.Testing.Acceptance.Resources
             OrderCount = 1
         }
 
+        /// <summary>
+        /// Method returning a valid MDX query if the param Measure is equal to OrderCount else an invalid MDX statement
+        /// </summary>
+        /// <param name="year">a calendar year without the 'CY'</param>
+        /// <param name="measure"></param>
+        /// <returns></returns>
         public string GetSelectMdxWithTwoParams(int year, Measure measure)
         {
             if (measure== Measure.OrderCount)

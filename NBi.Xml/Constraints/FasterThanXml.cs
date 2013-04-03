b4cabe30bb1d878962e65c1fdbb0730679c0e7b6
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-04-03 21:43:26 +0200

    Fixed an incorrect and useless DefaultValue for max-time-milliSeconds generating a crash in some environments -x86 ?-

--- NBi.Xml/Constraints/FasterThanXml.cs
+++ NBi.Xml/Constraints/FasterThanXml.cs
@@ -6,7 +6,6 @@ namespace NBi.Xml.Constraints
     public class FasterThanXml : AbstractConstraintXml
     {
         [XmlAttribute("max-time-milliSeconds")]
-        [DefaultValue(false)]
         public int MaxTimeMilliSeconds { get; set; }
 
         [XmlAttribute("clean-cache")]

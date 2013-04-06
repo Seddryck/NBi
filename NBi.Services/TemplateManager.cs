76b8c3d3a05cc445797182623fcbd107e5b5bbca
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-04-07 00:58:22 +0200

    Move Templates to NBi.Service and some code to the TemplateManager

--- NBi.Services/TemplateManager.cs
+++ NBi.Services/TemplateManager.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace NBi.Service
 {
     public class TemplateManager
     {
+        private const string TEMPLATE_DIRECTORY = "NBi.Service.Templates.";
+        private const string TEMPLATE_DEFAULT = "ExistsDimension";
+
         public TemplateManager()
         {
 
@@ -17,6 +23,58 @@ namespace NBi.Service
             {
                 tw.Write(content);
             }
+        }
+
+        public string[] GetEmbeddedLabels()
+        {
+            var resources = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            IEnumerable<string> labels = resources.Where(t => t.StartsWith(TEMPLATE_DIRECTORY) && t.EndsWith(".txt")).ToList();
+            labels = labels.Select(t => t.Replace(TEMPLATE_DIRECTORY, ""));
+            labels = labels.Select(t => t.Substring(0, t.Length - 4));
+            labels = labels.Select(t => SplitCamelCase(t));
+            return labels.ToArray();
+        }
+
+
+        private static string SplitCamelCase(string str)
+        {
+            return Regex.Replace(
+                Regex.Replace(
+                    str,
+                    @"(\P{Ll})(\P{Ll}\p{Ll})",
+                    "$1 $2"
+                ),
+                @"(\p{Ll})(\P{Ll})",
+                "$1 $2"
+            );
+        }
+
+        public string GetDefaultContent()
+        {
+            return GetEmbeddedTemplate(TEMPLATE_DEFAULT);
+        }
+
+        public string GetEmbeddedTemplate(string resourceName)
+        {
+            var value = string.Empty;           //Template
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(string.Format("{0}{1}.txt", TEMPLATE_DIRECTORY, resourceName)))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    value = reader.ReadToEnd();
+                }
+            }
+            return value;
+        }
+
+        public string GetExternalTemplate(string resourceName)
+        {
+            var tpl = string.Empty;           //Template
+            using (var stream = new StreamReader(resourceName))
+            {
+                tpl = stream.ReadToEnd();
+            }
+            return tpl;
         }
     }
 }

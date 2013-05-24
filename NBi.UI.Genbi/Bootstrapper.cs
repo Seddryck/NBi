c80bddc46200db4cd61b3f4dd74342cc15300c78
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-05-24 21:28:11 +0200

    Added a bootstrapper and an exception window

---
+++ NBi.UI.Genbi/Bootstrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using NBi.UI.Genbi.View;
+
+namespace NBi.UI.Genbi
+{
+    class Bootstrapper
+    {
+        /// <summary>
+        /// Boots the application.
+        /// </summary>
+        /// <param name="args">
+        /// Parameters for the application startup.
+        /// </param>
+        public void Boot(params string[] args)
+        {
+
+            //var transformationService = new TransformationService();
+            //var validationService = new ValidationService();
+            //var documentService = new DocumentService();
+            //var documentPresenterFactory = new DocumentPresenterFactory(documentService);
+            //var interactionManager = new InteractionManager();
+
+            //var masterPresenter = new MasterPresenter(documentPresenterFactory, validationService, transformationService, interactionManager);
+            var dispatcher = new GenbiDispatcher();
+            dispatcher.Initialize();
+
+
+            if (args != null && args.Length != 0)
+            {
+                var testSuiteToOpen = args[0];
+                //TODO dispatcher.Open(testSuiteToOpen);
+            }
+
+            Application.Run(dispatcher.GetMainForm());
+        }
+    }
+}

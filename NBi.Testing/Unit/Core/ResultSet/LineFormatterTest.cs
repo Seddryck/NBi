b0e25652b6ec57f84f7ecc499c7d13f39b6c6c67
C?dric Charlier <cedric.charlier@yucom.be>
2013-06-26 22:02:51 +0200

    Added (empty) as a placeholder to manage string with a length of 0

--- NBi.Testing/Unit/Core/ResultSet/LineFormatterTest.cs
+++ NBi.Testing/Unit/Core/ResultSet/LineFormatterTest.cs
@@ -31,5 +31,56 @@ namespace NBi.Testing.Unit.Core.ResultSet
             cf.GetText(4);
             Assert.Pass();
         }
+
+        [Test]
+        public void Build_NullAndEmptyAreDifferent_CorrectDisplay()
+        {
+            // Design Dummy Column
+            DataColumn col = new DataColumn("DummyColumn");
+            col.ExtendedProperties.Add("NBi::Role", ColumnRole.Key);
+            col.ExtendedProperties.Add("NBi::Type", ColumnType.Text);
+
+            // Design dummy table
+            DataTable table = new DataTable();
+            table.Columns.Add(col);
+
+            var row = table.NewRow();
+            row[0] = DBNull.Value;
+            row.SetColumnError(0, "(empty)");
+
+            ICellFormatter cf = LineFormatter.Build(row, 0);
+
+            // This must not throw an exception when the header is bigger that requested size
+            var text = cf.GetText(20);
+            Assert.That(text, Is.StringContaining("(null)"));
+            Assert.That(text, Is.StringContaining("<>"));
+            Assert.That(text, Is.StringContaining("(empty)"));
+        }
+
+        [Test]
+        public void Build_EmptyAndNullAreDifferent_CorrectDisplay()
+        {
+            // Design Dummy Column
+            DataColumn col = new DataColumn("DummyColumn");
+            col.ExtendedProperties.Add("NBi::Role", ColumnRole.Key);
+            col.ExtendedProperties.Add("NBi::Type", ColumnType.Text);
+
+            // Design dummy table
+            DataTable table = new DataTable();
+            table.Columns.Add(col);
+
+            var row = table.NewRow();
+            row[0] = string.Empty;
+            row.SetColumnError(0, "(null)");
+
+            ICellFormatter cf = LineFormatter.Build(row, 0);
+
+            // This must not throw an exception when the header is bigger that requested size
+            var text = cf.GetText(20);
+
+            Assert.That(text, Is.StringContaining("(empty)"));
+            Assert.That(text, Is.StringContaining("<>"));
+            Assert.That(text, Is.StringContaining("(null)")); 
+        }
     }
 }

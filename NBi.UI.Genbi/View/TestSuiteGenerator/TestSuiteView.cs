f12219a9d605c320dc49b068d8fe6e4614ee0ad5
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-08-03 23:32:25 +0200

    Removed useless code for UI

--- NBi.UI.Genbi/View/TestSuiteGenerator/TestSuiteView.cs
+++ NBi.UI.Genbi/View/TestSuiteGenerator/TestSuiteView.cs
@@ -12,7 +12,7 @@ using NBi.UI.Genbi.Stateful;
 
 namespace NBi.UI.Genbi.View.TestSuiteGenerator
 {
-    partial class TestSuiteView : Form, ITestCasesView, ITemplateView, ISettingsView, ITestsGenerationView, ITestSuiteView
+    partial class TestSuiteView : Form
     {
 
         private TestSuiteState State { get; set; }
@@ -20,17 +20,17 @@ namespace NBi.UI.Genbi.View.TestSuiteGenerator
         private TemplatePresenter TemplatePresenter { get; set; }
         private SettingsPresenter SettingsPresenter { get; set; }
         private TestListPresenter TestListPresenter { get; set; }
-        private TestSuitePresenter TestSuitePresenter { get; set; } 
+        private TestSuitePresenter TestSuitePresenter { get; set; }
 
 
         public TestSuiteView()
         {
             State = new TestSuiteState();
-            TestCasesPresenter = new TestCasesPresenter(this, new RenameVariableWindow(), new TestCasesManager(), State.TestCases, State.Variables);
-            TemplatePresenter = new TemplatePresenter(this, new TemplateManager(), State.Template);
-            SettingsPresenter = new SettingsPresenter(this, new SettingsManager(), State.Settings);
-            TestListPresenter = new TestListPresenter(this, new TestListManager(), State.Tests, State.TestCases, State.Variables, State.Template);
-            TestSuitePresenter = new TestSuitePresenter(this, new TestSuiteManager(), State.Tests, State.Settings);
+            TestCasesPresenter = new TestCasesPresenter(new RenameVariableWindow(), new TestCasesManager(), State.TestCases, State.Variables);
+            TemplatePresenter = new TemplatePresenter(new TemplateManager(), State.Template);
+            SettingsPresenter = new SettingsPresenter(new SettingsManager(), State.Settings);
+            TestListPresenter = new TestListPresenter(new TestListManager(), State.Tests, State.TestCases, State.Variables, State.Template);
+            TestSuitePresenter = new TestSuitePresenter(new TestSuiteManager(), State.Tests, State.Settings);
 
             InitializeComponent();
             DeclareBindings();            
@@ -54,23 +54,23 @@ namespace NBi.UI.Genbi.View.TestSuiteGenerator
 
         private void BindPresenter()
         {
+            //TestCases & Variables
             CommandManager.Instance.Bindings.Add(this.TestCasesPresenter.OpenTestCasesCommand, openTestCasesToolStripMenuItem);
             CommandManager.Instance.Bindings.Add(this.TestCasesPresenter.OpenTestCasesCommand, openTestCasesToolStripButton);
             CommandManager.Instance.Bindings.Add(this.TestCasesPresenter.RemoveVariableCommand, testCasesControl.RemoveCommand);
             CommandManager.Instance.Bindings.Add(this.TestCasesPresenter.RenameVariableCommand, testCasesControl.RenameCommand);
 
+            //Template
             CommandManager.Instance.Bindings.Add(this.TemplatePresenter.OpenTemplateCommand, openTemplateToolStripMenuItem);
             CommandManager.Instance.Bindings.Add(this.TemplatePresenter.OpenTemplateCommand, openTemplateToolStripButton);
             CommandManager.Instance.Bindings.Add(this.TemplatePresenter.SaveTemplateCommand, saveAsTemplateToolStripMenuItem);
             CommandManager.Instance.Bindings.Add(this.TemplatePresenter.SaveTemplateCommand, saveAsTemplateToolStripButton);
 
+            //Settings
             CommandManager.Instance.Bindings.Add(this.SettingsPresenter.AddReferenceCommand, settingsControl.AddCommand);
             CommandManager.Instance.Bindings.Add(this.SettingsPresenter.RemoveReferenceCommand, settingsControl.RemoveCommand);
 
-            //CommandManager.Instance.Bindings.Add(this.TestListPresenter.OpenTestSuiteCommand, openTestSuiteToolStripMenuItem);
-            //CommandManager.Instance.Bindings.Add(this.TestListPresenter.OpenTestSuiteCommand, openTestSuiteToolStripButton);
-
-
+            //Tests
             CommandManager.Instance.Bindings.Add(this.TestListPresenter.GenerateTestsXmlCommand, generateTestsToolStripMenuItem);
             CommandManager.Instance.Bindings.Add(this.TestListPresenter.GenerateTestsXmlCommand, generateTestsToolStripButton);
             CommandManager.Instance.Bindings.Add(this.TestListPresenter.ClearTestsXmlCommand, clearTestsToolStripMenuItem);
@@ -80,6 +80,7 @@ namespace NBi.UI.Genbi.View.TestSuiteGenerator
             CommandManager.Instance.Bindings.Add(this.TestListPresenter.DeleteTestCommand, testListControl.DeleteCommand);
             CommandManager.Instance.Bindings.Add(this.TestListPresenter.DisplayTestCommand, testListControl.DisplayCommand);
 
+            //Test-suite
             CommandManager.Instance.Bindings.Add(this.TestSuitePresenter.OpenTestSuiteCommand, openTestSuiteToolStripMenuItem);
             CommandManager.Instance.Bindings.Add(this.TestSuitePresenter.OpenTestSuiteCommand, openTestSuiteToolStripButton);
             CommandManager.Instance.Bindings.Add(this.TestSuitePresenter.SaveAsTestSuiteCommand, saveAsTestSuiteToolStripMenuItem);
@@ -94,124 +95,124 @@ namespace NBi.UI.Genbi.View.TestSuiteGenerator
 
         #region properties
 
-        public DataTable TestCases
-        {
-            get
-            {
-                return (DataTable)(testCasesControl.bindingCsv.DataSource);
-            }
-            set
-            {
-                testCasesControl.bindingCsv.DataSource = value;
-            }
-        }
-
-        public bool UseGrouping
-        {
-            get
-            {
-                return false;//templateControl.UseGrouping;
-            }
-            set
-            {
-                //templateControl.UseGrouping = value;
-            }
-        }
-
-
-        public BindingList<Test> Tests
-        {
-            get
-            {
-                return null;
-            }
-            set
-            {
-                //testListControl.Tests = value;
-            }
-        }
-
-        public string TemplateValue
-        {
-            get
-            {
-                return null;//templateControl.Template;
-            }
-            set
-            {
-                //templateControl.Template = value;
-            }
-        }
-
-
-        public string SettingsValue
-        {
-            get
-            {
-                return null; //settingsControl.Value;
-            }
-            set
-            {
-                //settingsControl.Value = value;
-            }
-        }
-
-
-        public BindingList<string> SettingsNames
-        {
-            get
-            {
-                return null; //settingsControl.Names;
-            }
-            set
-            {
-                //settingsControl.Names = value;
-            }
-        }
-
-        public int SettingsSelectedIndex
-        {
-            get
-            {
-                return 0;
-            }
-            set
-            {
-                
-            }
-        }
-
-        public Test TestSelection
-        {
-            get
-            {
-                return null; //testListControl.TestSelection;
-            }
-            set
-            {
-                //testListControl.TestSelection = value;
-            }
-        }
-
-        public int TestSelectedIndex
-        {
-            get
-            {
-                return 0;// testListControl.TestSelectedIndex;
-            }
-            set
-            {
-                //testListControl.TestSelectedIndex = value;
-            }
-        }
-
-        //public int ProgressValue
+        //public DataTable TestCases
         //{
+        //    get
+        //    {
+        //        return (DataTable)(testCasesControl.bindingCsv.DataSource);
+        //    }
         //    set
         //    {
-        //        testListControl.ProgressValue = value;
+        //        testCasesControl.bindingCsv.DataSource = value;
         //    }
         //}
+
+        //public bool UseGrouping
+        //{
+        //    get
+        //    {
+        //        return false;//templateControl.UseGrouping;
+        //    }
+        //    set
+        //    {
+        //        //templateControl.UseGrouping = value;
+        //    }
+        //}
+
+
+        //public BindingList<Test> Tests
+        //{
+        //    get
+        //    {
+        //        return null;
+        //    }
+        //    set
+        //    {
+        //        //testListControl.Tests = value;
+        //    }
+        //}
+
+        //public string TemplateValue
+        //{
+        //    get
+        //    {
+        //        return null;//templateControl.Template;
+        //    }
+        //    set
+        //    {
+        //        //templateControl.Template = value;
+        //    }
+        //}
+
+
+        //public string SettingsValue
+        //{
+        //    get
+        //    {
+        //        return null; //settingsControl.Value;
+        //    }
+        //    set
+        //    {
+        //        //settingsControl.Value = value;
+        //    }
+        //}
+
+
+        //public BindingList<string> SettingsNames
+        //{
+        //    get
+        //    {
+        //        return null; //settingsControl.Names;
+        //    }
+        //    set
+        //    {
+        //        //settingsControl.Names = value;
+        //    }
+        //}
+
+        //public int SettingsSelectedIndex
+        //{
+        //    get
+        //    {
+        //        return 0;
+        //    }
+        //    set
+        //    {
+                
+        //    }
+        //}
+
+        //public Test TestSelection
+        //{
+        //    get
+        //    {
+        //        return null; //testListControl.TestSelection;
+        //    }
+        //    set
+        //    {
+        //        //testListControl.TestSelection = value;
+        //    }
+        //}
+
+        //public int TestSelectedIndex
+        //{
+        //    get
+        //    {
+        //        return 0;// testListControl.TestSelectedIndex;
+        //    }
+        //    set
+        //    {
+        //        //testListControl.TestSelectedIndex = value;
+        //    }
+        //}
+
+        ////public int ProgressValue
+        ////{
+        ////    set
+        ////    {
+        ////        testListControl.ProgressValue = value;
+        ////    }
+        ////}
 
         #endregion
 
@@ -240,64 +241,6 @@ namespace NBi.UI.Genbi.View.TestSuiteGenerator
 
 
 
-        public BindingList<string> Variables
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        BindingList<Test> ITestSuiteView.Tests
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        Test ITestSuiteView.TestSelection
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        int ITestSuiteView.TestSelectedIndex
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        event EventHandler IView.Initialize
-        {
-            add { }
-            remove { throw new NotImplementedException(); }
-        }
-
-
-        public int VariableSelectedIndex
-        {
-            get { throw new NotImplementedException(); }
-        }
+        
     }
 }

48f3555c963ae4029401e60787917ed0f2f79347
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-08-13 14:13:10 +0200

    Tabifyed

--- NBi.NUnit.Runtime/ConnectionStringsFinder.cs
+++ NBi.NUnit.Runtime/ConnectionStringsFinder.cs
@@ -6,23 +6,23 @@ using System.Linq;
 
 namespace NBi.NUnit.Runtime
 {
-    public class ConnectionStringsFinder
-    {
-        protected internal virtual NameValueCollection Find()
-        {
-            var list = new NameValueCollection();
-            string configFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
-            //Try to find a config file, if existing take the path inside for the TestSuite
-            if (File.Exists(configFile))
-            {
-                //line bellow to avoid .Net framework bug: http://support.microsoft.com/kb/2580188/en-us
-                var configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+	public class ConnectionStringsFinder
+	{
+		protected internal virtual NameValueCollection Find()
+		{
+			var list = new NameValueCollection();
+			string configFile = AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
+			//Try to find a config file, if existing take the path inside for the TestSuite
+			if (File.Exists(configFile))
+			{
+				//line bellow to avoid .Net framework bug: http://support.microsoft.com/kb/2580188/en-us
+				var configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
-                if (configuration.ConnectionStrings != null)
-                    foreach (ConnectionStringSettings css in configuration.ConnectionStrings.ConnectionStrings)
-	                    list.Add(css.Name, css.ConnectionString);
-            }
-            return list;
-        }
-    }
+				if (configuration.ConnectionStrings != null)
+					foreach (ConnectionStringSettings css in configuration.ConnectionStrings.ConnectionStrings)
+						list.Add(css.Name, css.ConnectionString);
+			}
+			return list;
+		}
+	}
 }

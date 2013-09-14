e6f70cd8e0be900030b45ce7f4cbf9121250d1ea
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-09-14 02:25:17 +0200

    Implement Rounding for Numeric values

---
+++ NBi.Core/ResultSet/Comparer/Rounding.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Core.ResultSet.Comparer
+{
+    public class Rounding
+    {
+        public enum RoudingStyle
+        {
+            Undefined = 0,
+            Floor,
+            Ceiling,
+            Round
+        }
+
+        public string Step { get; private set; }
+        public RoudingStyle Style { get; private set; }
+
+        protected Rounding(string step, RoudingStyle style)
+        {
+            Step = step;
+            Style = style; 
+        }
+
+        protected double GetValue(double value, double step)
+        {
+            var remainder = Math.Abs(value) % step;
+
+            if ((Style == RoudingStyle.Ceiling && remainder>0) || (Style == RoudingStyle.Round && remainder > step / 2))
+                remainder -= step;
+
+            return (value - remainder) * Math.Sign(value);
+        }
+    }
+}
---
+++ NBi.Testing/Unit/Core/ResultSet/Comparer/NumericRoundingTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using NBi.Core.ResultSet.Comparer;
+using NUnit.Framework;
+
+namespace NBi.Testing.Unit.Core.ResultSet.Comparer
+{
+    [TestFixture]
+    public class NumericRoundingTest
+    {
+        #region SetUp & TearDown
+        //Called only at instance creation
+        [TestFixtureSetUp]
+        public void SetupMethods()
+        {
+
+        }
+
+        //Called only at instance destruction
+        [TestFixtureTearDown]
+        public void TearDownMethods()
+        {
+        }
+
+        //Called before each test
+        [SetUp]
+        public void SetupTest()
+        {
+        }
+
+        //Called after each test
+        [TearDown]
+        public void TearDownTest()
+        {
+        }
+        #endregion
+
+        [Test]
+        [TestCase(105, 20, Rounding.RoudingStyle.Floor, 100)]
+        [TestCase(105, 2, Rounding.RoudingStyle.Floor, 104)]
+        [TestCase(105, 2, Rounding.RoudingStyle.Round, 104)]
+        [TestCase(108, 10, Rounding.RoudingStyle.Round, 110)]
+        [TestCase(105, 20, Rounding.RoudingStyle.Ceiling, 120)]
+        [TestCase(105, 2, Rounding.RoudingStyle.Ceiling, 106)]
+        [TestCase(105, 5, Rounding.RoudingStyle.Floor, 105)]
+        [TestCase(105, 5, Rounding.RoudingStyle.Ceiling, 105)]
+        [TestCase(105, 5, Rounding.RoudingStyle.Round, 105)]
+        public void GetValue_ValueStepStyle_NewValue(decimal value, int step, Rounding.RoudingStyle roundingStyle, decimal newValue)
+        {
+            var rounder = new NumericRounding(step, roundingStyle);
+            
+            Assert.That(rounder.GetValue(value), Is.EqualTo(newValue));
+        }
+    }
+}

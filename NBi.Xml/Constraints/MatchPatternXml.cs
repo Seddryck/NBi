1f3b0a040db29cd9436b0d326a67c019d32e8901
Cedric L. Charlier <cedric.charlier@yucom.be>
2013-11-06 23:28:49 +0100

    Create constraint MatchPattern at xml level

---
+++ NBi.Xml/Constraints/MatchPatternXml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using NBi.Xml.Items;
+using NBi.Xml.Settings;
+
+namespace NBi.Xml.Constraints
+{
+    public class MatchPatternXml : AbstractConstraintXml
+    {
+
+        [XmlElement("regex")]
+        public string Regex { get; set; }
+
+        public MatchPatternXml()
+        {
+            
+        }
+
+    }
+}

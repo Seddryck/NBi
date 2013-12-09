1cbaf7b4c472c478573405ac000468c40b0977c3
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-12-09 22:10:49 +0100

    Add the currency and numeric format support

---
+++ NBi.Xml/Items/Format/CurrencyFormatXml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Serialization;
+using NBi.Core.Format;
+
+namespace NBi.Xml.Items.Format
+{
+    public class CurrencyFormatXml : NumericFormatXml, ICurrencyFormat
+    {
+        [XmlAttribute("currency-symbol")]
+        public string CurrencySymbol { get; set; }
+
+        [XmlAttribute("currency-pattern")]
+        public CurrencyPattern CurrencyPattern { get; set; }
+
+        public CurrencyFormatXml() : base()
+        {
+        }
+    }
+}
---
+++ NBi.Xml/Items/Format/NumericFormatXml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Serialization;
+using NBi.Core.Format;
+
+namespace NBi.Xml.Items.Format
+{
+    public class NumericFormatXml : INumericFormat
+    {
+        [XmlAttribute("decimal-digits")]
+        public int DecimalDigits { get; set; }
+
+        [XmlAttribute("decimal-separator")]
+        [DefaultValue(".")]
+        public string DecimalSeparator { get; set; }
+
+        [XmlAttribute("group-separator")]
+        [DefaultValue(",")]
+        public string GroupSeparator { get; set; }
+
+        public NumericFormatXml() : base()
+        {
+            DecimalSeparator = ".";
+            GroupSeparator = ",";
+        }
+    }
+}

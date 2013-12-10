903743c91b928d17c288f9c33409e55d49834d64
Cedric L. Charlier <cedric.charlier@yucom.be>
2013-12-11 00:47:52 +0100

    Added XmlElement for PredefinedItems

---
+++ NBi.Xml/Items/PredefinedItems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using NBi.Core.Members;
+
+namespace NBi.Xml.Items
+{
+    public class PredefinedItems
+    {
+        [XmlAttribute("type")]
+        public PredefinedMembers Type { get; set; }
+
+        [XmlAttribute("language")]
+        public string Language { get; set; }
+
+        public IEnumerable<string> GetItems()
+        {
+            var factory = new PredefinedMembersFactory();
+            return factory.Instantiate(Type, Language);
+        }
+    }
+}

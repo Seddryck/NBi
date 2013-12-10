62ce049a1a88dbaaf40f26368b330589ab8650e8
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-12-11 00:15:40 +0100

    Define enumeration for supported list of predefined members

---
+++ NBi.Core/Members/PredefinedMembers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace NBi.Core.Members
+{
+    public enum PredefinedMembers
+    {
+        [XmlEnum(Name = "weekdays")]
+        DaysOfWeek = 1,
+        [XmlEnum(Name = "months")]
+        MonthsOfYear = 2
+    }
+}

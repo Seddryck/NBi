b3f3fb4a45dd30839164e6200fcc4f0a6f701a76
Cedric L. Charlier <cedric.charlier@yucom.be>
2013-12-11 02:16:03 +0100

    Define implementation of the contract for a range of dates and a range of integer

---
+++ NBi.Core/Members/Ranges/IntegerRange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Core.Members.Ranges
+{
+    public class IntegerRange : IRange
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public int Step { get; set; }
+    }
+}

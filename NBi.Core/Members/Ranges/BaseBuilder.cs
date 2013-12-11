6b2dace85a592d60c4712addf034ffada40a725f
Cedric L. Charlier <cedric.charlier@yucom.be>
2013-12-11 02:16:29 +0100

    Set the contract and the base to Build a range of members

---
+++ NBi.Core/Members/Ranges/BaseBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace NBi.Core.Members.Ranges
+{
+    internal abstract class BaseBuilder : IRangeMembersBuilder
+    {
+        protected IRange Range { get; set; }
+        protected IEnumerable<string> Result { get; set; }
+        private bool isSetup = false;
+        private bool isBuild = false;
+
+        public void Setup(IRange range)
+        {
+            Result = null;
+            Range = range;
+            isBuild = false;
+            isSetup = true;
+        }       
+
+        public void Build()
+        {
+            if (!isSetup)
+                throw new InvalidOperationException();
+            InternalBuild();
+            isBuild = true;
+        }
+
+        protected abstract void InternalBuild();
+
+        public IEnumerable<string> GetResult()
+        {
+            if (!isBuild)
+                throw new InvalidOperationException();
+            return Result;
+        }
+    }
+}

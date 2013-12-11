fa07615789206f560f4cd17936181284048129fd
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-12-11 02:17:37 +0100

    Add the builder for an Integer range

---
+++ NBi.Core/Members/Ranges/IntegerRangeBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Core.Members.Ranges
+{
+    internal class IntegerRangeBuilder : BaseBuilder
+    {
+        protected new IntegerRange Range
+        {
+            get
+            {
+                return (IntegerRange)base.Range;
+            }
+        }
+
+        protected override void InternalBuild()
+        {
+            Result = Build(Range.Start, Range.End, Range.Step);
+        }
+
+        public IEnumerable<string> Build(int start, int end, int step)
+        {
+            var list = new List<string>();
+            for (int i = start; i <= end; i+=step)
+                list.Add(i.ToString());
+
+            return list;
+        }
+    }
+}

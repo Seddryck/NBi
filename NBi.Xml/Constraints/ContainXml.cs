90c6c78853e89f87cd2bf7867c63bba115faf970
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-12-17 23:07:32 +0100

    Fixed inheritance missing

--- NBi.Xml/Constraints/ContainXml.cs
+++ NBi.Xml/Constraints/ContainXml.cs
@@ -30,7 +30,5 @@ namespace NBi.Xml.Constraints
             }  
         }
 
-        [XmlElement("members")]
-        public MembersXml Members { get; set; }
     }
 }
--- NBi.Xml/Constraints/EquivalentToXml.cs
+++ NBi.Xml/Constraints/EquivalentToXml.cs
@@ -1,12 +1,12 @@
 ﻿using System;
+using System.Xml.Serialization;
 using NBi.Xml.Systems;
 
 namespace NBi.Xml.Constraints
 {
     public class EquivalentToXml : AbstractConstraintForCollectionXml
     {
-        [XmlElement("members")]
-        public MembersXml Members { get; set; }
+        
         
     }
 }

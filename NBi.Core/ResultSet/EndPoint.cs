e6b1c266cb612a72257c522f389c348076f3dd91
Cédric L. Charlier <cedric.charlier@yucom.be>
2013-12-21 13:32:39 +0100

    Fixed bug #10 about 'Comparaison of resultSet, returns twice the actual value when values don't match'

--- NBi.Core/ResultSet/EndPoint.cs
+++ NBi.Core/ResultSet/EndPoint.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace NBi.Core.ResultSet
 {
-    public class EndPoint
+    public abstract class EndPoint
     {
         public double Value { get; set; }
         public bool IsClosed { get; set; }
@@ -23,25 +23,84 @@ namespace NBi.Core.ResultSet
             IsClosed = isClosed;
         }
 
+        protected abstract string BoundSymbol { get; }
     }
 
-    public class EndPointClosed : EndPoint
+    public abstract class LeftEndPoint : EndPoint
     {
-        public EndPointClosed(double value)
+        public LeftEndPoint(double value, bool isClosed)
+            : base(value, isClosed)
+        {
+        }
+
+        protected override string BoundSymbol
+        {
+            get
+            {
+                return IsClosed ? "[" : "]";
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}{1}", BoundSymbol, Value.ToString());
+        }
+    }
+
+    public abstract class RightEndPoint : EndPoint
+    {
+        public RightEndPoint(double value, bool isClosed)
+            : base(value, isClosed)
+        {
+        }
+
+        protected override string BoundSymbol
+        {
+            get
+            {
+                return IsClosed ? "]" : "[";
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{1}{0}", BoundSymbol, Value.ToString());
+        }
+    }
+
+    public class LeftEndPointClosed : LeftEndPoint
+    {
+        public LeftEndPointClosed(double value)
             : base(value, true)
         {
         }
     }
 
-    public class EndPointOpen : EndPoint
+    public class LeftEndPointOpen : LeftEndPoint
     {
-        public EndPointOpen(double value)
+        public LeftEndPointOpen(double value)
             : base(value, false)
         {
         }
     }
 
-    public class LeftEndPointNegativeInfinity : EndPoint
+    public class RightEndPointClosed : RightEndPoint
+    {
+        public RightEndPointClosed(double value)
+            : base(value, true)
+        {
+        }
+    }
+
+    public class RightEndPointOpen : RightEndPoint
+    {
+        public RightEndPointOpen(double value)
+            : base(value, false)
+        {
+        }
+    }
+
+    public class LeftEndPointNegativeInfinity : LeftEndPoint
     {
         public LeftEndPointNegativeInfinity()
             : base(double.NegativeInfinity, true)
@@ -49,7 +108,7 @@ namespace NBi.Core.ResultSet
         }
     }
 
-    public class RightEndPointPositiveInfinity : EndPoint
+    public class RightEndPointPositiveInfinity : RightEndPoint
     {
         public RightEndPointPositiveInfinity()
             : base(double.PositiveInfinity, true)

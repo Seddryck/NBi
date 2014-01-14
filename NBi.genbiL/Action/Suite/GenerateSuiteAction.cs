12f525f897cd44c191743dcef2c0147b58582b38
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-01-15 00:17:37 +0100

    Added two action files

---
+++ NBi.genbiL/Action/Suite/GenerateSuiteAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace NBi.GenbiL.Action.Suite
+{
+    public class GenerateSuiteAction : ISuiteAction
+    {
+        public void Execute(GenerationState state)
+        {
+            state.List.Build(state.Template.Code, state.TestCases.Variables.ToArray(), state.TestCases.Content, false);
+            state.Suite.DefineSettings(state.Settings.GetSettings());
+            state.Suite.DefineTests(state.List.GetTests());
+        }
+    }
+}

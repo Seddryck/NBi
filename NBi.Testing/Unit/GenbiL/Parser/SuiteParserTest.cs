d9a6a6f2c88f4aca844308bbbaed3f964d1335ac
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-01-14 23:56:50 +0100

    Added unit tests for parsers

---
+++ NBi.Testing/Unit/GenbiL/Parser/SuiteParserTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using NBi.GenbiL.Action.Suite;
+using NBi.GenbiL.Parser;
+using NUnit.Framework;
+using Sprache;
+
+namespace NBi.Testing.Unit.GenbiL.Parser
+{
+    [TestFixture]
+    public class SuiteParserTest
+    {
+        [Test]
+        public void SentenceParser_SuiteGenerate_ValidGenerateSuiteAction()
+        {
+            var input = "suite generate;";
+            var result = Suite.Parser.Parse(input);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<GenerateSuiteAction>());
+        }
+
+        [Test]
+        public void SentenceParser_SuiteSaveString_ValidSaveSuiteAction()
+        {
+            var input = "suite save 'filename.nbits';";
+            var result = Suite.Parser.Parse(input);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<SaveSuiteAction>());
+            Assert.That(((SaveSuiteAction)result).Filename, Is.EqualTo("filename.nbits"));
+        }
+    }
+}

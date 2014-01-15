317134606197855b7e48b70ec91e3935098a3a1a
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-01-15 22:22:28 +0100

    Added Tokenization to support multiple speca between words

--- NBi.genbiL/Parser/Grammar.cs
+++ NBi.genbiL/Parser/Grammar.cs
@@ -8,10 +8,10 @@ namespace NBi.GenbiL.Parser
     class Grammar
     {
         public static readonly Parser<string> Textual = Parse.Letter.AtLeastOnce().Text().Token();
-        public static readonly Parser<string> BracketTextual = Parse.CharExcept("[]").AtLeastOnce().Text().Contained(Parse.Char('['), Parse.Char(']'));
-        public static readonly Parser<string> QuotedTextual = Parse.CharExcept("'").AtLeastOnce().Text().Contained(Parse.Char('\''), Parse.Char('\''));
+        public static readonly Parser<string> BracketTextual = Parse.CharExcept("[]").AtLeastOnce().Text().Contained(Parse.Char('['), Parse.Char(']')).Token();
+        public static readonly Parser<string> QuotedTextual = Parse.CharExcept("'").AtLeastOnce().Text().Contained(Parse.Char('\''), Parse.Char('\'')).Token();
         public static readonly Parser<string> Record = Textual.Or(BracketTextual);
         public static readonly Parser<IEnumerable<string>> RecordSequence = Record.DelimitedBy(Parse.Char(','));
-        public static readonly Parser<char> Terminator = Parse.Char(';');
+        public static readonly Parser<char> Terminator = Parse.Char(';').Token();
     }
 }

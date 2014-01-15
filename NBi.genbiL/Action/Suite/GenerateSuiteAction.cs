94a7fcc42a68ce88456d05df22f62d9ad2037422
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-01-15 22:20:30 +0100

    Fixed issue when generating a group suite from macro with grouping option

--- NBi.genbiL/Action/Suite/GenerateSuiteAction.cs
+++ NBi.genbiL/Action/Suite/GenerateSuiteAction.cs
@@ -14,7 +14,7 @@ namespace NBi.GenbiL.Action.Suite
 
         public void Execute(GenerationState state)
         {
-            state.List.Build(state.Template.Code, state.TestCases.Variables.ToArray(), state.TestCases.Content, false);
+            state.List.Build(state.Template.Code, state.TestCases.Variables.ToArray(), state.TestCases.Content, Grouping);
             state.Suite.DefineSettings(state.Settings.GetSettings());
             state.Suite.DefineTests(state.List.GetTests());
         }

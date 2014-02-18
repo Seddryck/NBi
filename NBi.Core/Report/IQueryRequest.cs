59bff2576d4737d5e076c8793fe7952ec8db1c0f
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-02-18 22:24:42 +0100

    Add methods in Core to retrieve queries from rdl files stored in the database of SQL Server

---
+++ NBi.Core/Report/IQueryRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Core.Report
+{
+    public interface IQueryRequest
+    {
+        string ConnectionString { get; }
+        string ReportPath { get; }
+        string ReportName { get; }
+        string DataSetName { get; }
+    }
+}

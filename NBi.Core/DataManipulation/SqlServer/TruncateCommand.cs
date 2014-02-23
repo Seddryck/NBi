f6ae2e08eb58f10ecbfb40f14db4762cf31d7b89
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-02-23 11:43:05 +0100

    Added truncate and bulkload commands in NBi.Core

---
+++ NBi.Core/DataManipulation/SqlServer/TruncateCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace NBi.Core.DataManipulation.SqlServer
+{
+	class TruncateCommand : IDataManipulationImplementation
+	{
+		private readonly string connectionString;
+		private readonly string tableName;
+
+		public TruncateCommand(IResetCommand command)
+		{
+			connectionString = command.ConnectionString;
+			tableName = command.TableName;
+		}
+
+		public void Execute()
+		{
+			Execute(connectionString, tableName);
+		}
+
+		internal void Execute(string connectionString, string tableName)
+		{           
+			using (var conn = new SqlConnection(connectionString))
+			{
+				var cmd = new SqlCommand();
+				cmd.Connection = conn;
+				cmd.CommandText = string.Format ("truncate table {0};", tableName);
+				cmd.Connection.Open();
+				cmd.ExecuteNonQuery();
+			}
+		}
+
+	}
+}

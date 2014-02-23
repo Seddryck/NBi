0c54eef35f81682f0b838c681cf681206070ba27
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-02-23 11:45:23 +0100

    Added tests for deserialization of setup and cleanup

---
+++ NBi.Testing/Unit/Xml/Decoration/DecorationXmlTest.cs
@@ -0,0 +1,79 @@
+﻿using System.IO;
+using System.Reflection;
+using NBi.Xml;
+using NBi.Xml.Constraints;
+using NBi.Xml.Decoration;
+using NUnit.Framework;
+
+namespace NBi.Testing.Unit.Xml.Decoration
+{
+    [TestFixture]
+    public class DecorationXmlTest
+    {
+
+        protected TestSuiteXml DeserializeSample()
+        {
+            // Declare an object variable of the type to be deserialized.
+            var manager = new XmlManager();
+
+            // A Stream is needed to read the XML document.
+            using (Stream stream = Assembly.GetExecutingAssembly()
+                                           .GetManifestResourceStream("NBi.Testing.Unit.Xml.Resources.DecorationXmlTestSuite.xml"))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                manager.Read(reader);
+            }
+            return manager.TestSuite;
+        }
+        
+        [Test]
+        public void Deserialize_SampleFile_Setup()
+        {
+            int testNr = 0;
+
+            // Create an instance of the XmlSerializer specifying type and namespace.
+            TestSuiteXml ts = DeserializeSample();
+
+            // Check the properties of the object.
+            Assert.That(ts.Tests[testNr].Setup, Is.TypeOf<SetupXml>());
+        }
+
+
+        [Test]
+        public void Deserialize_SampleFile_SetupCountCommands()
+        {
+            int testNr = 0;
+
+            // Create an instance of the XmlSerializer specifying type and namespace.
+            TestSuiteXml ts = DeserializeSample();
+
+            // Check the properties of the object.
+            Assert.That(ts.Tests[testNr].Setup.Commands, Has.Count.EqualTo(2));
+        }
+
+        [Test]
+        public void Deserialize_SampleFile_Cleanup()
+        {
+            int testNr = 0;
+
+            // Create an instance of the XmlSerializer specifying type and namespace.
+            TestSuiteXml ts = DeserializeSample();
+
+            // Check the properties of the object.
+            Assert.That(ts.Tests[testNr].Cleanup, Is.TypeOf<CleanupXml>());
+        }
+
+        [Test]
+        public void Deserialize_SampleFile_CleanupCountCommands()
+        {
+            int testNr = 0;
+
+            // Create an instance of the XmlSerializer specifying type and namespace.
+            TestSuiteXml ts = DeserializeSample();
+
+            // Check the properties of the object.
+            Assert.That(ts.Tests[testNr].Cleanup.Commands, Has.Count.EqualTo(1));
+        }
+
+    }
+}

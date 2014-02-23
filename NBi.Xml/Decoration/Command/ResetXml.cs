b8f7fac48b39ef1510d18c39abb146d222fc120a
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-02-23 11:44:05 +0100

    Added cleanup, setup and supported commands to the Xml serialization

---
+++ NBi.Xml/Decoration/Command/ResetXml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Serialization;
+using NBi.Core.DataManipulation;
+
+namespace NBi.Xml.Decoration.Command
+{
+    public class ResetXml: DecorationCommandXml, IResetCommand
+    {
+        [XmlAttribute("table")]
+        public string TableName {get; set;}
+        
+    }
+}

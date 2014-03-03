665ff0fc56bb7ccf2e916a6a3e61a4094d622f1e
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-03-03 14:07:40 +0100

    Added Evaluation of rows in NBi.Core

---
+++ NBi.Core/Evaluate/IEvaluationRowsDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Core.Evaluate
+{
+    public interface IEvaluationRowsDefinition
+    {
+        List<IColumnVariable> Variables { get; }
+        List<IColumnExpression> Expressions { get; }
+    }
+}

458b64dcac0c967d4f5835fed33530159a2f4bb8
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-03-05 14:56:40 +0100

    Added Integration tests for all the setup/cleanup/check commands and predicates

---
+++ NBi.Testing/Integration/Core/DataManipulation/SqlServer/BulkLoadCommandTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using Moq;
+using NBi.Core.DataManipulation;
+using NBi.Core.DataManipulation.SqlServer;
+using NUnit.Framework;
+
+namespace NBi.Testing.Integration.Core.DataManipulation.SqlServer
+{
+    [TestFixture]
+    [Category("Local SQL instance")]
+    public class BulkLoadCommandTest
+	{
+		private const string FILE_CSV = "Load.csv";
+        private string FileName { get; set;}
+
+        
+        public void CreateTemporaryTable(string tableName, string connectionString)
+        {
+            //Build the fullpath for the file to read
+            FileName = DiskOnFile.CreatePhysicalFile(FILE_CSV, "NBi.Testing.Integration.Core.DataManipulation." + FILE_CSV);
+
+            //Create the table
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                conn.Open();
+                cmd.CommandText = string.Format("if (exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA = 'dbo' and TABLE_NAME = '{0}')) begin drop table dbo.{0}; end", tableName);
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = string.Format("create table {0} (Id int IDENTITY(1,1), Name varchar (255));", tableName);
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = string.Format("insert into {0} values ('No name');", tableName);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private int CountElementsInTable(string tableName, string connectionString)
+        {
+            int count = -1;
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = string.Format("select count(*) from {0};", tableName);
+                conn.Open();
+                count = (int) cmd.ExecuteScalar();
+            }
+            return count;
+        }
+
+        [Test]
+		public void Execute_LoadTemporaryTableBasedOnCSV_TableHasMoreData()
+		{
+            //Create table
+            CreateTemporaryTable("Temporary", ConnectionStringReader.GetLocalSqlClient());
+
+            //Check how many elements are available in the table
+            var before = CountElementsInTable("Temporary", ConnectionStringReader.GetLocalSqlClient());
+
+             //Mock the commandXml
+            var info = Mock.Of<ILoadCommand>(
+                command => command.ConnectionString== ConnectionStringReader.GetLocalSqlClient()
+			        && command.TableName=="Temporary"
+                    && command.FileName == FileName
+                );
+
+            //Apply the test
+			var cmd = new BulkLoadCommand(info);
+            cmd.Execute();
+
+            //Execute Query on temporary table to knwo the new count of elements
+            var after = CountElementsInTable("Temporary", ConnectionStringReader.GetLocalSqlClient());
+
+            Assert.That(after, Is.GreaterThan(before));
+		}
+
+	}
+}
---
+++ NBi.Testing/Integration/Core/DataManipulation/SqlServer/TruncateCommandTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using Moq;
+using NBi.Core.DataManipulation;
+using NBi.Core.DataManipulation.SqlServer;
+using NUnit.Framework;
+
+namespace NBi.Testing.Integration.Core.DataManipulation.SqlServer
+{
+    [TestFixture]
+    [Category("Local SQL instance")]
+    public class TruncateCommandTest
+	{
+        private int CountElementsInTable(string tableName, string connectionString)
+        {
+            int count = -1;
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = string.Format("select count(*) from {0};", tableName);
+                conn.Open();
+                count = (int)cmd.ExecuteScalar();
+            }
+            return count;
+        }
+
+        public void CreateTemporaryTable(string tableName, string connectionString)
+        {
+            //Create the table
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                conn.Open();
+                cmd.CommandText = string.Format("if (exists (select * from INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA = 'dbo' and TABLE_NAME = '{0}')) begin drop table dbo.{0}; end", tableName);
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = string.Format("create table {0} (Id int IDENTITY(1,1), Name varchar (255));", tableName);
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = string.Format("insert into {0} values ('No name');", tableName);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        [Test]
+        public void Execute_TruncateTemporaryTable_TableIsEmpty()
+        {
+            //Create table
+            CreateTemporaryTable("Temporary", ConnectionStringReader.GetLocalSqlClient());
+
+            //Mock the commandXml
+            var info = Mock.Of<IResetCommand>(
+                command => command.ConnectionString == ConnectionStringReader.GetLocalSqlClient()
+                    && command.TableName == "Temporary"
+                );
+
+            //Apply the test
+            var cmd = new TruncateCommand(info);
+            cmd.Execute();
+
+            //Execute Query on temporary table to knwo the new count of elements
+            var after = CountElementsInTable("Temporary", ConnectionStringReader.GetLocalSqlClient());
+
+            Assert.That(after, Is.EqualTo(0));
+        }
+
+	}
+}

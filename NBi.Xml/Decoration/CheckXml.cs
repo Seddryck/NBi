8630f004af1d770be1b0209ae69bbd580249dd5a
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-03-05 14:54:53 +0100

    Added the notion of Check in addition to setup and cleanup

---
+++ NBi.Xml/Decoration/CheckXml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using NBi.Xml.Decoration.Check;
+using NBi.Xml.Decoration.Command;
+
+namespace NBi.Xml.Decoration
+{
+    public class CheckXml
+    {
+        [XmlElement(Type = typeof(ServiceRunningXml), ElementName = "service-running")
+        ]
+        public List<DecorationCheckXml> Predicates { get; set; }
+
+        public CheckXml()
+        {
+            Predicates = new List<DecorationCheckXml>();
+        }
+    }
+}

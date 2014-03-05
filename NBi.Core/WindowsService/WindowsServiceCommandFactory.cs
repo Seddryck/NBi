9074a3e30a3d823af81f29dc1fda68620907afaa
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-03-05 14:53:24 +0100

    Added service-start and service-stop as new commands in setup and cleanup

---
+++ NBi.Core/WindowsService/WindowsServiceCommandFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Core.WindowsService
+{
+    class WindowsServiceCommandFactory
+    {
+        public WindowsServiceCommand Get(IWindowsServiceCommand command)
+        {
+            if (command is IWindowsServiceStartCommand)
+                return WindowsServiceCommand.Start(command);
+                                
+            if (command is IWindowsServiceStopCommand)
+                return WindowsServiceCommand.Stop(command);
+
+            throw new ArgumentException();
+        }
+    }
+}

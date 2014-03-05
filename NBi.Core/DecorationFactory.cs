8dc54a093b447c9cb28fff83b2b0c5274f59d57a
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-03-05 14:56:08 +0100

    Added common factory for check and setup/cleanup

---
+++ NBi.Core/DecorationFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using NBi.Core.DataManipulation;
+using NBi.Core.WindowsService;
+
+namespace NBi.Core
+{
+    public class DecorationFactory
+    {
+        public IDecorationCommandImplementation Get(IDecorationCommand command)
+        {
+            if (command is IWindowsServiceCommand)
+            {
+                return new WindowsServiceCommandFactory().Get(command as IWindowsServiceCommand);
+            }
+
+            if (command is IDataManipulationCommand)
+            {
+                return new DataManipulationFactory().Get(command as IDataManipulationCommand);
+            }
+
+            throw new ArgumentException();
+        }
+
+        public IDecorationCheckImplementation Get(IDecorationCheck check)
+        {
+            if (check is IWindowsServiceCheck)
+            {
+                return new WindowsServiceCheckFactory().Get(check as IWindowsServiceCheck);
+            }
+
+            throw new ArgumentException();
+        }
+
+    }
+}

cd8318ba83177f3ca83e63d1c038b9fa478b11ce
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-03-11 21:40:59 +0100

    Added classes implementing the interfaces for etl into Xml

---
+++ NBi.Xml/Decoration/Command/EtlRunXml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using NBi.Core.Etl;
+using NBi.Xml.Items;
+
+namespace NBi.Xml.Decoration.Command
+{
+    public class EtlRunXml : DecorationCommandXml, IEtlRunCommand
+    {
+        [XmlAttribute("server")]
+        public string Server { get; set; }
+
+        [XmlAttribute("path")]
+        public string Path { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+
+        [XmlElement("parameter")]
+        public List<EtlParameter> Parameters { get; set; }
+
+        public EtlRunXml()
+        {
+            Parameters = new List<EtlParameter>();
+        }
+    }
+}
---
+++ NBi.Xml/Items/EtlXml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+using NBi.Core.Etl;
+
+namespace NBi.Xml.Items
+{
+    public class EtlXml: IEtl
+    {
+        [XmlAttribute("server")]
+        public string Server { get; set; }
+
+        [XmlAttribute("path")]
+        public string Path { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlElement("parameter")]
+        public List<EtlParameter> Parameters { get; set; }
+
+        public EtlXml()
+        {
+            Parameters = new List<EtlParameter>();
+        }
+    }
+}

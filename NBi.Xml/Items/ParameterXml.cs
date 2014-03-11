0dfe08321860697f1813429f92ef4ce9f0facbdf
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-03-11 21:44:47 +0100

    Added inheritence for QueryParameterXml

---
+++ NBi.Xml/Items/ParameterXml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Serialization;
+using NBi.Core.Query;
+
+namespace NBi.Xml.Items
+{
+    public abstract class ParameterXml
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlText]
+        public string StringValue { get; set; }
+
+        public virtual T GetValue<T>()
+        {
+            TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+            if (converter != null)
+            {
+                return (T)converter.ConvertFrom(StringValue);
+            }
+            else
+                throw new InvalidOperationException();
+        }
+    }
+}

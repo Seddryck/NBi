ba04ce12422b27939bcd0710e09b7706534d7b48
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-03-11 21:41:46 +0100

    Added the interfaces for Etl ETlRunner and the corresponding result

---
+++ NBi.Core/Etl/EtlRunResult.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace NBi.Core.Etl
+{
+    public class EtlRunResult
+    {
+        public bool IsSuccess { get; private set; }
+        public bool IsFailure
+        {
+            get
+            {
+                return !IsSuccess;
+            }
+        }
+
+        public string Message { get; private set; }
+
+        protected EtlRunResult(bool isSuccess)
+        {
+            IsSuccess = isSuccess;
+        }
+
+        public static EtlRunResult Build(DTSExecResult result, string message)
+        {
+            switch (result)
+            {
+                case DTSExecResult.Failure:
+                    return Failure(message);
+                case DTSExecResult.Success:
+                    return Success();
+                default:
+                    break;
+            }
+            throw new ArgumentException();
+        }
+
+        public static EtlRunResult Failure(string message)
+        {
+            return new EtlRunResult(false) {Message=message};
+        }
+
+        public static EtlRunResult Success()
+        {
+            return new EtlRunResult(true);
+        }
+    }
+}

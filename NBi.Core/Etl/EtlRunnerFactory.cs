e68bc56e9b8f70a4aee553aa5497d5177cdea4d2
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-03-11 21:42:58 +0100

    Create facorties to build the effective runner

---
+++ NBi.Core/Etl/EtlRunnerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using NBi.Core.Etl.IntegrationService;
+
+namespace NBi.Core.Etl
+{
+    public class EtlRunnerFactory
+    {
+        public IEtlRunner Get(IEtl etl)
+        {
+            var factory = new SsisEtlRunnerFactory();
+            var runner = factory.Get(etl);
+            return runner;
+        }
+    }
+}

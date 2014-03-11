ed85bca08c6ffc224359a40f58d4d775c7c713c6
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-03-11 21:42:17 +0100

    Effective implementations of runners for Sql Server

---
+++ NBi.Core/Etl/IntegrationService/EtlFileRunner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace NBi.Core.Etl.IntegrationService
+{
+    class EtlFileRunner : EtlRunner
+    {
+        public EtlFileRunner(IEtl etl) : base(etl)
+        {
+
+        }
+
+        protected override Package Load(IEtl etl, Application app)
+        {
+            var packageName = etl.Path + etl.Name;
+            if (!packageName.ToLower().EndsWith(".dtsx"))
+                packageName += ".dtsx";
+
+            var events = new PackageEvents();
+            var package = app.LoadPackage(packageName, events);
+            return package;                
+        }
+    }
+}
---
+++ NBi.Core/Etl/IntegrationService/EtlRunner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace NBi.Core.Etl.IntegrationService
+{
+    abstract class EtlRunner: IEtlRunner
+    {
+        public IEtl Etl { get; private set; }
+
+        public EtlRunner(IEtl etl)
+        {
+            Etl = etl;
+        }
+
+        public EtlRunResult Execute()
+        {
+            var app = new Application();
+            var package = Load(Etl, app);
+
+            Parameterize(Etl.Parameters, ref package);
+
+            var events = new PackageEvents();
+            var pkgResults = package.Execute(null, null, events, null, null);
+            return EtlRunResult.Build(pkgResults, events.Message);
+
+        }
+
+        protected abstract Package Load(IEtl etl, Application app);
+
+        protected virtual void Parameterize(IEnumerable<EtlParameter> parameters, ref Package package)
+        {
+            foreach (var param in parameters)
+            {
+                package.Parameters[param.Name].Value = param.StringValue;
+            }
+        }
+    }
+}

fb65f02a384028ea2cd999b32de5f35c71862518
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-03-11 21:42:17 +0100

    Effective implementations of runners for Sql Server

---
+++ NBi.Core/Etl/IntegrationService/EtlSqlServerRunner.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace NBi.Core.Etl.IntegrationService
+{
+    class EtlSqlServerRunner : EtlRunner
+    {
+        public EtlSqlServerRunner(IEtl etl)
+            : base(etl)
+        {
+
+        }
+
+        protected override Package Load(IEtl etl, Application app)
+        {
+            var server = etl.Server.Replace(" ", "");
+            if(server.ToLower()=="(local)")
+                server=".";
+
+            var packageName = etl.Path + etl.Name;
+            if (!packageName.ToLower().EndsWith(".dtsx"))
+                packageName += ".dtsx";
+            
+            var package = app.LoadFromDtsServer(packageName, server, null);
+            return package;                
+        }
+    }
+}

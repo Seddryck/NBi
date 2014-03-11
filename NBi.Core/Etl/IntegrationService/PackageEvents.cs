e7c82f9cfd28e02250b031c90039537add7d74ab
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-03-11 21:42:40 +0100

    Added support to capture events in case of errors during package execution

---
+++ NBi.Core/Etl/IntegrationService/PackageEvents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace NBi.Core.Etl.IntegrationService
+{
+    class PackageEvents : DefaultEvents
+    {
+        public string Message { get; set; }
+
+        public override bool OnError(DtsObject source, int errorCode, string subComponent, string description, string helpFile, int helpContext, string idofInterfaceWithError)
+        {
+            Message = string.Format("{0}: {1}", subComponent, description);
+            return base.OnError(source, errorCode, subComponent, description, helpFile, helpContext, idofInterfaceWithError);
+        }
+    }
+}

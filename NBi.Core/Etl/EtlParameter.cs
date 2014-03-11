282c7107acf544d849a52159be8a78456fd3548b
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-03-11 21:39:21 +0100

    Define interfaces for an Etl

---
+++ NBi.Core/Etl/EtlParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Core.Etl
+{
+    public class EtlParameter
+    {
+        public virtual string StringValue { get; set; }
+        public virtual string Name { get; set; }
+    }
+}

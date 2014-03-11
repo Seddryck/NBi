733bf7245bf1902736bd784c16388b4b2538fa07
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-03-11 23:41:05 +0100

    Tested the two new constraints

---
+++ NBi.Testing/Unit/NUnit/Execution/SuccessfulConstraintTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Moq;
+using NBi.Core;
+using NBi.NUnit.Execution;
+using NUnit.Framework;
+
+namespace NBi.Testing.Unit.NUnit.Execution
+{
+    [TestFixture]
+    public class SuccessfulConstraintTest
+    {
+        
+        #region Setup & Teardown
+
+        [SetUp]
+        public void SetUp()
+        {
+           
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        #endregion
+
+        [Test]
+        public void Matches_IsSuccessful_True()
+        {
+            var stub = new Mock<IExecution>();
+            stub.Setup(e => e.Execute())
+                .Returns(Mock.Of<IExecutionResult>(r => r.IsSuccess == true));
+            var engine = stub.Object;
+
+            var successfulConstraint = new SuccessfulConstraint();
+
+            Assert.That(successfulConstraint.Matches(engine), Is.True);
+        }
+
+        [Test]
+        public void Matches_IsFailure_False()
+        {
+            var stub = new Mock<IExecution>();
+            stub.Setup(e => e.Execute())
+                .Returns(Mock.Of<IExecutionResult>(r => r.IsSuccess == false));
+            var engine = stub.Object;
+
+            var successfulConstraint = new SuccessfulConstraint();
+
+            Assert.That(successfulConstraint.Matches(engine), Is.False);
+        }
+    }
+}

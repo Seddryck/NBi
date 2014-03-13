972d35b29ad7e8d334a92349662481c5f4108789
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-03-13 23:27:43 +0100

    Added support of parameters and template-varaibles to the Quaryable class in place of Query

--- NBi.Xml/Items/QueryableXml.cs
+++ NBi.Xml/Items/QueryableXml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Serialization;
 
@@ -7,5 +8,36 @@ namespace NBi.Xml.Items
     public abstract class QueryableXml : BaseItem
     {       
         public abstract string GetQuery();
+
+        public QueryableXml()
+        {
+            Parameters = new List<QueryParameterXml>();
+        }
+
+        [XmlElement("parameter")]
+        public List<QueryParameterXml> Parameters { get; set; }
+
+        [XmlElement("variable")]
+        public List<QueryTemplateVariableXml> Variables { get; set; }
+
+        public virtual List<QueryParameterXml> GetParameters()
+        {
+            var list = Parameters;
+            foreach (var param in Default.Parameters)
+                if (!Parameters.Exists(p => p.Name == param.Name))
+                    list.Add(param);
+
+            return list;
+        }
+
+        public virtual List<QueryTemplateVariableXml> GetVariables()
+        {
+            var list = Variables;
+            foreach (var variable in Default.Variables)
+                if (!Variables.Exists(p => p.Name == variable.Name))
+                    list.Add(variable);
+
+            return list;
+        }
     }
 }

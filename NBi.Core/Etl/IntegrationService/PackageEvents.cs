0ef60ef9b95fd08d5030af4c42f99c77d42aaea5
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-05-13 00:01:17 +0200

    Introduced packageEvents to calculate execution timesapn

--- NBi.Core/Etl/IntegrationService/PackageEvents.cs
+++ NBi.Core/Etl/IntegrationService/PackageEvents.cs
@@ -4,14 +4,28 @@ using Microsoft.SqlServer.Dts.Runtime;
 
 namespace NBi.Core.Etl.IntegrationService
 {
-    class PackageEvents : DefaultEvents
+    class PackageEvents : DefaultEvents, IPackageEvents
     {
         public string Message { get; set; }
+        private DateTime StartTime { get; set; }
+        public TimeSpan ExecutionTime { get; set; }
 
         public override bool OnError(DtsObject source, int errorCode, string subComponent, string description, string helpFile, int helpContext, string idofInterfaceWithError)
         {
             Message = string.Format("{0}: {1}", subComponent, description);
             return base.OnError(source, errorCode, subComponent, description, helpFile, helpContext, idofInterfaceWithError);
+        }
+
+        public override void OnPreExecute(Executable exec, ref bool fireAgain)
+        {
+            StartTime = DateTime.Now;
+            base.OnPreExecute(exec, ref fireAgain);
+        }
+
+        public override void OnPostExecute(Executable exec, ref bool fireAgain)
+        {
+            ExecutionTime = DateTime.Now.Subtract(StartTime);
+            base.OnPreExecute(exec, ref fireAgain);
         }
     }
 }

bbbde4c6807f1c8fc37aae5c2f77360115171d46
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-05-13 00:00:32 +0200

    Refactored parameter

--- NBi.Core/Etl/EtlParameter.cs
+++ NBi.Core/Etl/EtlParameter.cs
@@ -1,13 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Xml.Serialization;
 
 namespace NBi.Core.Etl
 {
     public class EtlParameter
     {
+        [XmlIgnore]
         public virtual string StringValue { get; set; }
+        [XmlIgnore]
         public virtual string Name { get; set; }
+
+        public EtlParameter()
+        { }
+
+        public EtlParameter(string name, object value)
+            : this()
+        {
+            Name = name;
+            StringValue = value.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is EtlParameter))
+                return false;
+            
+            var objParam = obj as EtlParameter;
+            return (objParam.Name == this.Name && objParam.StringValue == this.StringValue);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() * StringValue.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Etl parameter name={0} ; value={1}", Name, StringValue);
+        }
     }
 }

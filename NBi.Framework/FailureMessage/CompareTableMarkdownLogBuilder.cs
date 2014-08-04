3f2911f8e84b30c0d791cba1b5a0f90bc8b7ea09
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-08-04 18:58:54 +0200

    Refactored to support an override to display comparison in the table

---
+++ NBi.Framework/FailureMessage/CompareTableMarkdownLogBuilder.cs
@@ -0,0 +1,46 @@
+﻿using MarkdownLog;
+using NBi.Core.ResultSet;
+using NBi.Core.ResultSet.Comparer;
+using NBi.Unit.Framework.FailureMessage.Formatter;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Framework.FailureMessage
+{
+    public class CompareTableMarkdownLogBuilder : TableMarkdownLogBuilder
+    {
+        protected override List<TableRow> BuildRows(IEnumerable<DataRow> dataRows, List<ColumnType> columnTypes)
+        {
+            var rows = new List<TableRow>();
+            foreach (DataRow dataRow in dataRows)
+            {
+                var cells = new List<TableCell>();
+                for (int i = 0; i < dataRow.Table.Columns.Count; i++)
+                {
+                    var text = GetText(columnTypes, dataRow, i);
+                    var compared = GetCompareText(columnTypes, dataRow, i);
+                    var fullText = string.Format("{0}{1}{2}", text, string.IsNullOrEmpty(compared) ? "" : " <> ", compared);
+                    cells.Add(new TableCell() { Text = fullText });
+                }
+                rows.Add(new TableRow() { Cells = cells });
+            }
+            return rows;
+        }
+
+        protected string GetCompareText(List<ColumnType> columnTypes, DataRow dataRow, int i)
+        {
+            if (string.IsNullOrEmpty(dataRow.GetColumnError(i)))
+                return string.Empty;
+            
+            var factory = new CellFormatterFactory();
+            var formatter = factory.GetObject(columnTypes[i]);
+
+            return formatter.Format(dataRow.GetColumnError(i));
+        }
+    }
+}

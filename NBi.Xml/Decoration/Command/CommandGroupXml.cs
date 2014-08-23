784f624da3859d7adf977b20f5601258f7a866a1
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-08-23 20:50:33 +0200

    Added a feature to parallelize commands during setup and cleanup

--- NBi.Xml/Decoration/Command/CommandGroupXml.cs
+++ NBi.Xml/Decoration/Command/CommandGroupXml.cs
@@ -1,25 +1,30 @@
-﻿using System;
+﻿using NBi.Xml.Decoration.Command;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Text;
 using System.Xml.Serialization;
-using NBi.Xml.Decoration.Command;
 
-namespace NBi.Xml.Decoration
+namespace NBi.Xml.Decoration.Command
 {
-    public abstract class DecorationXml
+    public class CommandGroupXml : DecorationCommandXml
     {
         [XmlElement(Type = typeof(TableLoadXml), ElementName = "table-load"),
         XmlElement(Type = typeof(TableResetXml), ElementName = "table-reset"),
         XmlElement(Type = typeof(ServiceStartXml), ElementName = "service-start"),
         XmlElement(Type = typeof(ServiceStopXml), ElementName = "service-stop"),
-        XmlElement(Type = typeof(EtlRunXml), ElementName = "etl-run"),
-        XmlElement(Type = typeof(CommandGroupXml), ElementName = "tasks")
+        XmlElement(Type = typeof(EtlRunXml), ElementName = "etl-run")
         ]
         public List<DecorationCommandXml> Commands { get; set; }
 
+        [DefaultValue(true)]
+        [XmlAttribute("parallel")]
+        public bool Parallel { get; set; }
 
-        public DecorationXml()
+        public CommandGroupXml()
         {
+            Parallel = true;
             Commands = new List<DecorationCommandXml>();
         }
     }

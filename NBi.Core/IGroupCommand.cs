1718d632314ac4559705f686463da0bc10045baa
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-08-23 20:51:16 +0200

    Implemented the effective implemnattion of parallelization of commands

---
+++ NBi.Core/IGroupCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NBi.Core
+{
+    public interface IGroupCommand : IDecorationCommand
+    {
+        List<IDecorationCommand> Commands { get; set; }
+        bool Parallel { get; set; }
+    }
+}

6f5b281577f3e2cc4b468cbad60172c99ad7f14c
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-08-24 19:04:40 +0200

    Changed the construction of DML commands to not build twice the IDbConnection

--- NBi.Core/DataManipulation/IDataManipulationFactory.cs
+++ NBi.Core/DataManipulation/IDataManipulationFactory.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 
 namespace NBi.Core.DataManipulation
 {
     interface IDataManipulationFactory
     {
-        IDecorationCommandImplementation Get(IDataManipulationCommand command);
+        IDecorationCommandImplementation Get(IDataManipulationCommand command, IDbConnection connection);
     }
 }

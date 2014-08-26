f62bfe1bfb137ba163db029b5439cff4cc0d7ffe
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-08-26 23:20:54 +0200

    Updated DatabaseParser to support Shared dataset

---
+++ NBi.Core/Report/ReportServerQueryFromSharedDataSet.sql
@@ -0,0 +1,28 @@
+﻿WITH cte
+AS (
+	SELECT 
+		[path]
+		, [name] AS SharedDataSetName,
+		CONVERT(XML, CONVERT(VARBINARY(MAX), content)) AS rdl
+	FROM
+		dbo.catalog
+	WHERE
+		type=8
+),
+SharedDataSet AS (
+SELECT
+	LEFT([Path], LEN([path]) - CHARINDEX('/',REVERSE([Path])) + 1) AS ReportPath
+	, SharedDataSetName AS SharedDataSetName
+	, ISNULL(T1.N.value('(*:CommandType/text())[1]', 'nvarchar(128)'), 'T-SQL') AS CommandType
+	, T1.N.value('(*:CommandText/text())[1]', 'nvarchar(max)') AS CommandText
+FROM
+	cte AS T
+CROSS APPLY
+	T.rdl.nodes('/*:SharedDataSet/*:DataSet/*:Query') AS T1 (N)
+)
+SELECT
+	*
+FROM
+	SharedDataSet
+WHERE
+	SharedDataSetName=@SharedDataSetName

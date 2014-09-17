b95100be5bc7603c0019fd2791a944ffb80c5a0a
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-09-17 18:24:10 +0200

    Renamed Move by Copy

--- NBi.Xml/Decoration/DecorationXml.cs
+++ NBi.Xml/Decoration/DecorationXml.cs
@@ -13,7 +13,7 @@ namespace NBi.Xml.Decoration
         XmlElement(Type = typeof(ServiceStartXml), ElementName = "service-start"),
         XmlElement(Type = typeof(ServiceStopXml), ElementName = "service-stop"),
         XmlElement(Type = typeof(FileDeleteXml), ElementName = "file-delete"),
-        XmlElement(Type = typeof(FileMoveXml), ElementName = "file-move"),
+        XmlElement(Type = typeof(FileCopyXml), ElementName = "file-copy"),
         XmlElement(Type = typeof(EtlRunXml), ElementName = "etl-run"),
         XmlElement(Type = typeof(CommandGroupXml), ElementName = "tasks")
         ]

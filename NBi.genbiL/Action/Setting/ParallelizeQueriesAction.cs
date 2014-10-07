ff961fc0c542ff52f9896ea49733eb2a8ff162a5
Cédric L. Charlier <cedric.charlier@yucom.be>
2014-10-07 23:30:04 +0200

    Add control of Parallelize-Queries in genbiL

---
+++ NBi.genbiL/Action/Setting/ParallelizeQueriesAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using NBi.Xml.Settings;
+
+namespace NBi.GenbiL.Action.Setting
+{
+    public class ParallelizeQueriesAction : ISettingAction
+    {
+        public bool Value { get; set; }
+
+        public ParallelizeQueriesAction(bool value)
+        {
+            Value = value;
+        }
+
+        public void Execute(GenerationState state)
+        {
+            state.Settings.Settings.ParallelizeQueries = Value;
+        }
+
+        public string Display
+        {
+            get
+            {
+                return string.Format("Set parameter 'parallellize queries' to '{0}'."
+                    , Value ? "ON" : "OFF"
+                    );
+            }
+        }
+    }
+}
---
+++ NBi.genbiL/Action/Setting/ParameterActionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using NBi.Xml.Settings;
+
+namespace NBi.GenbiL.Action.Setting
+{
+    public class ParameterActionFactory 
+    {
+        public ParameterActionFactory()
+        {
+        }
+
+        public ISettingAction Build(string name, bool value)
+        {
+            if (name.ToLower()=="parallelize-queries")
+                return new ParallelizeQueriesAction(value);
+            throw new ArgumentOutOfRangeException();
+        }
+
+    }
+}

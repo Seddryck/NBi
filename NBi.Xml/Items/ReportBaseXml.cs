793082e851924d8148a81cecf7432bb36c28d0b0
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-12-13 21:52:03 +0100

    Add abstraction for Report

---
+++ NBi.Xml/Items/ReportBaseXml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Xml.Serialization;
+using NBi.Core;
+using NBi.Core.Report;
+
+namespace NBi.Xml.Items
+{
+    public class ReportBaseXml : QueryableXml
+    {
+        [XmlAttribute("source")]
+        public string Source { get; set; }
+        
+        [XmlAttribute("path")]
+        public string Path { get; set; }
+
+        public override string GetQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

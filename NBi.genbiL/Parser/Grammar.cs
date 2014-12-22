a2efe4fbdfdb549dbebe5124d5a59fc7903172ed
Cedric L. Charlier <cedric.charlier@yucom.be>
2014-12-22 23:21:22 +0100

    Add parsing for columns/values

--- NBi.genbiL/Parser/Grammar.cs
+++ NBi.genbiL/Parser/Grammar.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Sprache;
+using NBi.GenbiL.Parser.Valuable;
 
 namespace NBi.GenbiL.Parser
 {
@@ -18,6 +19,16 @@ namespace NBi.GenbiL.Parser
         public static readonly Parser<IEnumerable<string>> RecordSequence = Record.DelimitedBy(Parse.Char(','));
         public static readonly Parser<IEnumerable<string>> QuotedRecordSequence = QuotedTextual.DelimitedBy(Parse.Char(','));
         public static readonly Parser<IEnumerable<string>> ExtendedQuotedRecordSequence = ExtendedQuotedTextual.DelimitedBy(Parse.Char(','));
+        public static readonly Parser<ValuableType> ValuableColumn = Parse.IgnoreCase("Columns").Or(Parse.IgnoreCase("Column")).Return(ValuableType.Column).Token();
+        public static readonly Parser<ValuableType> ValuableValue = Parse.IgnoreCase("Values").Or(Parse.IgnoreCase("Value")).Return(ValuableType.Value).Token();
+        public static readonly Parser<ValuableType> ValuableClass = ValuableColumn.Or(ValuableValue);
+        public static readonly Parser<IEnumerable<IValuable>> Valuables = 
+        (
+            from valuableClass in Grammar.ValuableClass
+            from items in Grammar.ExtendedQuotedRecordSequence
+            select new ValuableBuilder().Build(valuableClass, items)
+        );
+
         public static readonly Parser<char> Terminator = Parse.Char(';').Token();
         public static readonly Parser<bool> Boolean = Parse.IgnoreCase("on").Return(true)
                                                         .Or(Parse.IgnoreCase("yes").Return(true))

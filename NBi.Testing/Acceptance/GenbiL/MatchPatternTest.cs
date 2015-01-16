bca13981b52766512f72d87ae1ec09530a3ffb2b
Cedric L. Charlier <cedric.charlier@yucom.be>
2015-01-16 17:41:32 +0100

    Add new tests for the desrialization of numeric-format

---
+++ NBi.Testing/Acceptance/GenbiL/MatchPatternTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NBi.GenbiL;
+using NUnit.Framework;
+
+namespace NBi.Testing.Acceptance.GenbiL
+{
+    [TestFixture]
+    public class MatchPatternTest
+    {
+        private const string TEST_SUITE_NAME="MatchPattern";
+        private string DefinitionFilename { get { return "Acceptance\\GenbiL\\Resources\\" + TEST_SUITE_NAME + ".genbil"; } }
+        private string TargetFilename { get { return "Acceptance\\GenbiL\\Resources\\" + TEST_SUITE_NAME + ".nbits"; } }
+        //private string CsvFilename { get { return "Acceptance\\GenbiL\\Resources\\" + TEST_SUITE_NAME + ".csv"; } }
+
+        #region SetUp & TearDown
+        //Called only at instance creation
+        [TestFixtureSetUp]
+        public void SetupMethods()
+        {
+
+        }
+
+        //Called only at instance destruction
+        [TestFixtureTearDown]
+        public void TearDownMethods()
+        {
+            
+        }
+
+        //Called before each test
+        [SetUp]
+        public void SetupTest()
+        {
+            if (File.Exists(TargetFilename))
+                File.Delete(TargetFilename);
+
+            //if(File.Exists(CsvFilename))
+            //    File.Delete(CsvFilename);
+        }
+
+        //Called after each test
+        [TearDown]
+        public void TearDownTest()
+        {
+            if (File.Exists(TargetFilename))
+                File.Delete(TargetFilename);
+        }
+        #endregion
+
+        [Test]
+        public void Execute_MatchPattern_FileGenerated()
+        {
+            var generator = new TestSuiteGenerator();
+            generator.Load(DefinitionFilename);
+            generator.Execute();
+
+            Assert.That(File.Exists(TargetFilename));
+        }
+
+        [Test]
+        public void Execute_MatchPattern_NumberFormatSerialized()
+        {
+            var generator = new TestSuiteGenerator();
+            generator.Load(DefinitionFilename);
+            generator.Execute();
+
+            if (!File.Exists(TargetFilename))
+                Assert.Inconclusive("Test Suite not generated!");
+
+            var content = File.ReadAllText(TargetFilename);
+            Assert.That(content, Is.StringContaining("<matchPattern"));
+            Assert.That(content, Is.StringContaining("<numeric-format"));
+            Assert.That(content, Is.StringContaining("decimal-digits=\"3\""));
+        }
+
+    }
+}

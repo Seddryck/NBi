0168fee25d574b2e2ccc1fe2301afb8d69a24237
Cedric L. Charlier <cedric.charlier@yucom.be>
2015-02-07 22:50:58 +0100

    Improve reporting of xml validation failures

--- NBi.Testing/Unit/Xml/XmlManagerTest.cs
+++ NBi.Testing/Unit/Xml/XmlManagerTest.cs
@@ -38,5 +38,38 @@ namespace NBi.Testing.Unit.Xml
             var manager = new XmlManager();
             Assert.Throws<ArgumentException>(delegate { manager.Load(filename); });
         }
+
+        [Test]
+        public void Load_InvalidFile_ExceptionHasCorrectInformation()
+        {
+            var filename = DiskOnFile.CreatePhysicalFile("TestSuiteInvalidSyntax.xml", "NBi.Testing.Unit.Xml.Resources.XmlManagerInvalidSyntax.xml");
+
+            var manager = new XmlManager();
+            var exception = Assert.Throws<ArgumentException>(delegate { manager.Load(filename); });
+            Assert.That(exception.Message, Is.StringContaining("1 error has been found during the validation of the test-suite"));
+            Assert.That(exception.Message, Is.StringContaining("\tAt line 4: The element 'test' in namespace 'http://NBi/TestSuite' has invalid child element 'syntacticallyCorrect' in namespace 'http://NBi/TestSuite'."));
+        }
+
+        [Test]
+        public void Load_InvalidMultipleFile_ThrowException()
+        {
+            var filename = DiskOnFile.CreatePhysicalFile("TestSuiteInvalidSyntaxMultiple.xml", "NBi.Testing.Unit.Xml.Resources.XmlManagerInvalidSyntaxMultiple.xml");
+
+            var manager = new XmlManager();
+            Assert.Throws<ArgumentException>(delegate { manager.Load(filename); });
+        }
+
+        [Test]
+        public void Load_InvalidMultipleFile_ExceptionHasCorrectInformation()
+        {
+            var filename = DiskOnFile.CreatePhysicalFile("TestSuiteInvalidSyntaxMultiple.xml", "NBi.Testing.Unit.Xml.Resources.XmlManagerInvalidSyntaxMultiple.xml");
+
+            var manager = new XmlManager();
+            var exception = Assert.Throws<ArgumentException>(delegate { manager.Load(filename); });
+            Assert.That(exception.Message, Is.StringContaining("3 errors have been found during the validation of the test-suite"));
+            Assert.That(exception.Message, Is.StringContaining("At line 6: The element 'execution' in namespace 'http://NBi/TestSuite' has invalid child element 'sql' in namespace 'http://NBi/TestSuite'."));
+            Assert.That(exception.Message, Is.StringContaining("At line 11: The 'name' attribute is not declared."));
+            Assert.That(exception.Message, Is.StringContaining("At line 11: The 'http://NBi/TestSuite:less-than' element is invalid - The value 'alpha' is invalid according to its datatype 'http://NBi/TestSuite:more-less-type' - The string 'alpha' is not a valid Integer value."));
+        }
     }
 }

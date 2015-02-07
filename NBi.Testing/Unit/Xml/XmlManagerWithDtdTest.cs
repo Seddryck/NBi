3aa0c691647c6b09ff3d4ae5cb05c8da47272000
Cédric L. Charlier <cedric.charlier@yucom.be>
2015-02-08 00:41:25 +0100

    Review of the post-build events to include processed XSD in the Framework and exclude the unprocessed XSD

--- NBi.Testing/Unit/Xml/XmlManagerWithDtdTest.cs
+++ NBi.Testing/Unit/Xml/XmlManagerWithDtdTest.cs
@@ -13,7 +13,7 @@ namespace NBi.Testing.Unit.Xml
         [SetUp]
         public void Setup()
         {
-            var includedFilename = DiskOnFile.CreatePhysicalFile("TestSuiteIncludedTestSuite.nbiinclude", "NBi.Testing.Unit.Xml.Resources.TestSuiteIncludedTestSuite.xml");
+            var includedFilename = DiskOnFile.CreatePhysicalFile("TestSuiteIncludedTestSuite.xml", "NBi.Testing.Unit.Xml.Resources.TestSuiteIncludedTestSuite.xml");
             Console.WriteLine("Included file created at '{0}'", includedFilename);
             filename = DiskOnFile.CreatePhysicalFile("TestSuiteWithIncludeTestSuite.nbits", "NBi.Testing.Unit.Xml.Resources.TestSuiteWithIncludeTestSuite.xml");
             Console.WriteLine("Main file created at '{0}'", filename);

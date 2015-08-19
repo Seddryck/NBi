b2f6448cf6ad988aa2aa39d4952ae388ab5fb540
Cédric L. Charlier <cedric.charlier@yucom.be>
2015-08-20 00:37:01 +0200

    Serialization of xml-source

---
+++ NBi.Xml/Items/Xml/XmlSourceXml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NBi.Xml.Items.Xml
+{
+    public class XmlSourceXml : BaseItem
+    {
+        [XmlElement("file")]
+        public FileXml File { get; set; }
+
+        [XmlElement("xpath")]
+        public XPathXml XPath { get; set; }
+    }
+}

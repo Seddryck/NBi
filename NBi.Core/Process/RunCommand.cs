01f39730b120925443899a880377f62a4cf00efc
Cédric L. Charlier <cedric.charlier@yucom.be>
2015-12-13 20:23:15 +0100

    Improve messages when starting a process

--- NBi.Core/Process/RunCommand.cs
+++ NBi.Core/Process/RunCommand.cs
@@ -21,6 +21,10 @@ namespace NBi.Core.Process
 
         public void Execute()
         {
+            if (string.IsNullOrEmpty(argument))
+                Console.WriteLine("Starting process {0} without argument.", fullPath);
+            else
+                Console.WriteLine("Starting process {0} with arguments \"{1}\".", fullPath, argument);
             var result = false;
             var startInfo = new ProcessStartInfo();
             startInfo.FileName = fullPath;
@@ -29,13 +33,25 @@ namespace NBi.Core.Process
             {
                 if (timeOut != 0)
                 {
+                    Console.WriteLine("Waiting the end of the process.");
                     exeProcess.WaitForExit(timeOut);
                     if (exeProcess.HasExited)
+                    {
+                        if (exeProcess.ExitCode == 0)
+                            Console.WriteLine("Process has been successfully executed.");
+                        else
+                            Console.WriteLine("Process has failed.");
                         result = exeProcess.ExitCode == 0;
+                    }
+                    else
+                        Console.WriteLine("Process has been interrupted before the end of its execution.");
+                        
                 }
                 else
+                {
+                    Console.WriteLine("Not waiting the end of the process.");
                     result = true;
-                
+                }
             }
 
             if (!result)

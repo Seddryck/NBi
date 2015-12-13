2d68864cdd6fd5c7bf0ef687e046b768b2852e34
Cédric L. Charlier <cedric.charlier@yucom.be>
2015-12-13 17:39:41 +0100

    Better handling of exceptions when trying to connect Power BI Desktop

--- NBi.Core/PowerBiDesktop/PowerBiDesktopConnectionStringBuilder.cs
+++ NBi.Core/PowerBiDesktop/PowerBiDesktopConnectionStringBuilder.cs
@@ -28,7 +28,31 @@ namespace NBi.Core.PowerBiDesktop
         protected virtual string BuildLocalConnectionString(string name)
         {
             var processes = System.Diagnostics.Process.GetProcessesByName("msmdsrv");
-            var process = processes.FirstOrDefault(p => p.GetParent().MainWindowTitle == string.Format("{0} - Power BI Desktop", name));
+            if (processes==null || processes.Count()==0)
+            {
+                throw new ConnectionException
+                        (
+                            new InvalidOperationException("No process found with the name 'msmdsrv'. Are you sure your Power BI desktop solution is running?")
+                            , string.Format("PBIX = {0}", name)
+                        );
+            }
+                
+            var parentName = string.Format("{0} - Power BI Desktop", name);
+            var process = processes.FirstOrDefault(p => p.GetParent().MainWindowTitle == parentName);
+            if (process==null)
+            {
+                var existingParentNameString = new StringBuilder();
+                foreach (var p in processes)
+		            existingParentNameString.AppendFormat("'{0}', '", p.GetParent().MainWindowTitle);
+	            existingParentNameString = existingParentNameString.Remove(existingParentNameString.Length - 2, 2);
+                
+                throw new ConnectionException
+                    (
+                        new NullReferenceException(string.Format("No parent process found with the name '{0}'. Existing parent names were {1}.", name, existingParentNameString))
+                        , string.Format("PBIX = {0}", name)
+                    );
+            }
+
             var cmdLine = process.GetCommandLine();
             var workspace = GetWorkspace(cmdLine);
             var portFile = string.Format("{0}\\msmdsrv.port.txt", workspace);

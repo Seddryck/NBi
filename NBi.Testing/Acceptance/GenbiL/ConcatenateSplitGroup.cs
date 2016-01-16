6a9fb558180843f32ab79fa434ae44049594a3fc
Cédric L. Charlier <cedric.charlier@yucom.be>
2016-01-16 17:47:54 +0100

    Try to reproduce issue

---
+++ NBi.Testing/Acceptance/GenbiL/ConcatenateSplitGroup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NBi.GenbiL;
+using NUnit.Framework;
+
+namespace NBi.Testing.Acceptance.GenbiL
+{
+    [TestFixture]
+    public class ConcatenateSplitGroupTest
+    {
+        private const string TEST_SUITE_NAME = "ConcatenateSplitGroup";
+        private string DefinitionFilename { get { return "Acceptance\\GenbiL\\Resources\\" + TEST_SUITE_NAME + ".genbil"; } }
+        private string TargetFilename { get { return "Acceptance\\GenbiL\\Resources\\" + TEST_SUITE_NAME + ".nbits"; } }
+
+        #region SetUp & TearDown
+        //Called only at instance creation
+        [TestFixtureSetUp]
+        public void SetupMethods()
+        {
+
+        }
+
+        //Called only at instance destruction
+        [TestFixtureTearDown]
+        public void TearDownMethods()
+        {
+            
+        }
+
+        //Called before each test
+        [SetUp]
+        public void SetupTest()
+        {
+            if (File.Exists(TargetFilename))
+                File.Delete(TargetFilename);
+
+            //if(File.Exists(CsvFilename))
+            //    File.Delete(CsvFilename);
+        }
+
+        //Called after each test
+        [TearDown]
+        public void TearDownTest()
+        {
+            if (File.Exists(TargetFilename))
+                File.Delete(TargetFilename);
+        }
+        #endregion
+
+        [Test]
+        public void Execute_Group_FileGenerated()
+        {
+            var generator = new TestSuiteGenerator();
+            generator.Load(DefinitionFilename);
+            generator.Execute();
+
+            Assert.That(File.Exists(TargetFilename));
+        }
+
+
+    }
+}

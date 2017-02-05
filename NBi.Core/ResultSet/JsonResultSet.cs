be7c4d0656644b482db164c3b8156f487a78267f
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-02-05 15:20:35 +0100

    Json converter

---
+++ NBi.Core/ResultSet/JsonResultSet.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.ResultSet
+{
+    public class JsonResultSet
+    {
+        public ResultSet Build(string json)
+        {
+            var dt = (DataTable)JsonConvert.DeserializeObject(json, (typeof(DataTable)));
+
+            var isArrayConverted = false;
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                var column = dt.Columns[i];
+                if (column.DataType == typeof(DataTable))
+                {
+                    if (isArrayConverted)
+                        throw new InvalidOperationException("NBi can't convert a JSON document with more than one array to a result-set.");
+
+                    isArrayConverted = true;
+                    var newColumns = new List<DataColumn>();
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        var rowTable = (row.ItemArray[column.Ordinal]) as DataTable;
+                        if (rowTable!=null) //could be the case if the array is empty
+                            foreach (DataColumn subColumn in rowTable.Columns)
+                            {
+                                var columnName = string.Format("{0}.{1}", column.ColumnName, subColumn.ColumnName);
+                                if (!newColumns.Exists(c => c.ColumnName == columnName))
+                                {
+                                    var newColumn = new DataColumn()
+                                    {
+                                        ColumnName = columnName,
+                                        DataType = subColumn.DataType,
+                                        AllowDBNull = true,
+                                        DefaultValue = DBNull.Value
+                                    };
+                                    newColumn.ExtendedProperties.Add("split", subColumn.ColumnName);
+                                    newColumns.Add(newColumn);
+                                }
+                            }
+                    }
+
+                    var j = 1;
+                    foreach (var newColumn in newColumns)
+                    {
+                        dt.Columns.Add(newColumn);
+                        newColumn.SetOrdinal(column.Ordinal + j);
+                        j++;
+                    }
+
+                    var k = 0;
+                    while(k<dt.Rows.Count)
+                    {
+                        var masterRow = dt.Rows[k];
+                        var subTable = (masterRow.ItemArray[column.Ordinal]) as DataTable;
+                        var l = 1;
+                        foreach (DataRow rowTable in subTable.Rows)
+                        {
+                            var newRow = dt.NewRow();
+                            foreach (DataColumn columnIter in dt.Columns)
+                            {
+                                if (column != columnIter && !columnIter.ExtendedProperties.ContainsKey("split"))
+                                {
+                                    newRow[columnIter] = masterRow[columnIter];
+                                }
+                                else if (column != columnIter && columnIter.ExtendedProperties.ContainsKey("split"))
+                                {
+                                    var columnName = (string)columnIter.ExtendedProperties["split"];
+                                    if (rowTable.Table.Columns.Contains(columnName))
+                                        newRow[columnIter] = rowTable[columnName];
+                                }
+                            }
+                            dt.Rows.InsertAt(newRow, k+l);
+                            l++;
+                        }
+                        if (l>1) //If the array is empty we don't need to remove the row!
+                            dt.Rows.RemoveAt(k);
+
+                        k += l;
+                    }
+
+                    dt.Columns.Remove(column);
+
+                    foreach (DataColumn columnIter in dt.Columns)
+                    {
+                        if (columnIter.ExtendedProperties.ContainsKey("split"))
+                            columnIter.ExtendedProperties.Remove("split");
+                    }
+                }
+            }
+
+
+            var rs = new ResultSet();
+            rs.table = dt;
+
+
+            return rs;
+        }
+    }
+}

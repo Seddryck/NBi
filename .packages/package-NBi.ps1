5ea066b4f08c5571dc7fbce52cf5c4a0c7deee1b
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-03-19 00:14:11 +0100

    Add version as parameter

--- .packages/package-NBi.ps1
+++ .packages/package-NBi.ps1
@@ -1,5 +1,9 @@
+param(
+    [parameter(Mandatory=$true)]
+    [string]$version
+)
+
 $root = (split-path -parent $MyInvocation.MyCommand.Definition)
-$env:GitVersion_NuGetVersion = "1.15.0.0"
 
 #For NBi.Framework (dll)
 $lib = "$root\NBi.Framework\lib\45\"
@@ -12,14 +16,14 @@ new-item -Path $root\..\.nupkg -ItemType directory -force
 Copy-Item $root\..\NBi.NUnit.Runtime\bin\Debug\*.dll $lib
 
 
-Write-Host "Setting .nuspec version tag to $env:GitVersion_NuGetVersion"
+Write-Host "Setting .nuspec version tag to $version"
 
 $content = (Get-Content $root\NBi.Framework\NBi.Framework.nuspec -Encoding UTF8) 
-$content = $content -replace '\$version\$',$env:GitVersion_NuGetVersion
+$content = $content -replace '\$version\$',$version
 
 $content | Out-File $root\NBi.Framework\NBi.Framework.compiled.nuspec -Encoding UTF8
 
-& $root\..\.nuget\NuGet.exe pack $root\..\.packages\NBi.Framework\NBi.Framework.compiled.nuspec -Version $env:GitVersion_NuGetVersion -OutputDirectory $root\..\.nupkg
+& $root\..\.nuget\NuGet.exe pack $root\..\.packages\NBi.Framework\NBi.Framework.compiled.nuspec -Version $version -OutputDirectory $root\..\.nupkg
 
 #For NBi.Framework.Tools
 $lib = "$root\NBi.Framework.Tools\tools\"
@@ -31,11 +35,11 @@ new-item -Path $lib -ItemType directory
 new-item -Path $root\..\.nupkg -ItemType directory -force
 Copy-Item $root\..\NBi.NUnit.Runtime\bin\Debug\*.dll $lib
 
-Write-Host "Setting .nuspec version tag to $env:GitVersion_NuGetVersion"
+Write-Host "Setting .nuspec version tag to $version"
 
 $content = (Get-Content $root\NBi.Framework.Tools\NBi.Framework.Tools.nuspec -Encoding UTF8) 
-$content = $content -replace '\$version\$',$env:GitVersion_NuGetVersion
+$content = $content -replace '\$version\$',$version
 
 $content | Out-File $root\NBi.Framework.Tools\NBi.Framework.Tools.compiled.nuspec -Encoding UTF8
 
-& $root\..\.nuget\NuGet.exe pack $root\..\.packages\NBi.Framework.Tools\NBi.Framework.Tools.compiled.nuspec -Version $env:GitVersion_NuGetVersion -OutputDirectory $root\..\.nupkg
+& $root\..\.nuget\NuGet.exe pack $root\..\.packages\NBi.Framework.Tools\NBi.Framework.Tools.compiled.nuspec -Version $version -OutputDirectory $root\..\.nupkg

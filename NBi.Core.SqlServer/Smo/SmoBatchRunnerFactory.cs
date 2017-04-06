1ec1f3ce23ceb182e9dcfb56ef6d3b95e0810154
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-04-06 12:29:21 +0200

    Add handlng of invalid arguments when instanciating a SmoBatchRunner

--- NBi.Core.SqlServer/Smo/SmoBatchRunnerFactory.cs
+++ NBi.Core.SqlServer/Smo/SmoBatchRunnerFactory.cs
@@ -14,8 +14,19 @@ namespace NBi.Core.SqlServer.Smo
         public IDecorationCommandImplementation Get(IBatchCommand command, IDbConnection connection)
         {
             if (command is IBatchRunCommand)
+            {
+                if (connection == null)
+                    throw new ArgumentNullException("connection");
+                if (connection is SqlConnection)
+                    throw new ArgumentException(
+                        String.Format("To execute a SQL Batch on a SQL Server, you must provide a connection-string that is associated to a '{0}'. The connection-string '{1}' is associated to a '{2}'"
+                            , typeof(SqlConnection).AssemblyQualifiedName
+                            , connection.ConnectionString
+                            , connection.GetType().AssemblyQualifiedName)
+                        , "connection");
                 return new BatchRunCommand(command as IBatchRunCommand, connection as SqlConnection);
-
+            }
+                
             throw new ArgumentException();
         }
     }

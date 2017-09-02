f34d9d916a571f57da51974a00905dfe51a6d95d
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-09-02 18:56:34 +0200

    Update GitVersion config

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following 
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 // associated with an assembly.
 [assembly: AssemblyCompany("NBi Team - Cédric L. Charlier")]
 [assembly: AssemblyProduct("NBi")]
-[assembly: AssemblyCopyright("Copyright © Cédric L. Charlier 2012-2015")]
+[assembly: AssemblyCopyright("Copyright © Cédric L. Charlier 2012-2017")]
 [assembly: AssemblyDescription("NBi is a testing framework (add-on to NUnit) for Microsoft Business Intelligence platform and Data Access. The main goal of this framework is to let users create tests with a declarative approach based on an Xml syntax. By the means of NBi, you don't need to develop C# code to specify your tests! Either, you don't need Visual Studio to compile your test suite. Just create an Xml file and let the framework interpret it and play your tests. The framework is designed as an add-on of NUnit but with the possibility to port it easily to other testing frameworks.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -23,5 +23,7 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.14.0.*")]
+// [assembly: AssemblyVersion("1.16.0.0")]
+[assembly: AssemblyVersion("1.16")]
+[assembly: AssemblyFileVersion("1.16.0")]
+[assembly: AssemblyInformationalVersion("1.16.0-perso")]

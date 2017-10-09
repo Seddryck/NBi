2b2116b86abcadd1e3996626e8e92384fea6b504
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-10-09 21:45:21 +0200

    Fix issue when testing equality of double

--- NBi.Testing/Unit/Core/ResultSet/Comparer/TextToleranceFactoryTest.cs
+++ NBi.Testing/Unit/Core/ResultSet/Comparer/TextToleranceFactoryTest.cs
@@ -16,7 +16,7 @@ namespace NBi.Testing.Unit.Core.ResultSet.Comparer
             var tolerance = new TextToleranceFactory().Instantiate("JaccardDistance(0.8)");
             Assert.That(tolerance, Is.TypeOf<TextTolerance>());
             Assert.That(tolerance.Style, Is.EqualTo("Jaccard distance"));
-            Assert.That(tolerance.Value, Is.EqualTo(0.8));
+            Assert.That(tolerance.Value, Is.EqualTo(0.8).Within(0.001));
             Assert.That(tolerance.Implementation, Is.Not.Null);
             Assert.That(tolerance.Implementation("alpha", "alpha"), Is.EqualTo(0));
         }
@@ -27,7 +27,7 @@ namespace NBi.Testing.Unit.Core.ResultSet.Comparer
             var tolerance = new TextToleranceFactory().Instantiate("jaccard disTance(0.8)");
             Assert.That(tolerance, Is.TypeOf<TextTolerance>());
             Assert.That(tolerance.Style, Is.EqualTo("Jaccard distance"));
-            Assert.That(tolerance.Value, Is.EqualTo(0.8));
+            Assert.That(tolerance.Value, Is.EqualTo(0.8).Within(0.001));
             Assert.That(tolerance.Implementation, Is.Not.Null);
             Assert.That(tolerance.Implementation("alpha", "alpha"), Is.EqualTo(0));
         }
@@ -38,7 +38,7 @@ namespace NBi.Testing.Unit.Core.ResultSet.Comparer
             var tolerance = new TextToleranceFactory().Instantiate("LevenshteinDistance(0.8)");
             Assert.That(tolerance, Is.TypeOf<TextTolerance>());
             Assert.That(tolerance.Style, Is.EqualTo("Levenshtein distance"));
-            Assert.That(tolerance.Value, Is.EqualTo(0.8));
+            Assert.That(tolerance.Value, Is.EqualTo(0.8).Within(0.001));
             Assert.That(tolerance.Implementation, Is.Not.Null);
             Assert.That(tolerance.Implementation("alpha", "alpha"), Is.EqualTo(0));
         }
@@ -49,7 +49,7 @@ namespace NBi.Testing.Unit.Core.ResultSet.Comparer
             var tolerance = new TextToleranceFactory().Instantiate("Hamming(0.8)");
             Assert.That(tolerance, Is.TypeOf<TextTolerance>());
             Assert.That(tolerance.Style, Is.EqualTo("Hamming distance"));
-            Assert.That(tolerance.Value, Is.EqualTo(0.8));
+            Assert.That(tolerance.Value, Is.EqualTo(0.8).Within(0.001));
             Assert.That(tolerance.Implementation, Is.Not.Null);
             Assert.That(tolerance.Implementation("alpha", "alpha"), Is.EqualTo(0));
         }
@@ -60,7 +60,7 @@ namespace NBi.Testing.Unit.Core.ResultSet.Comparer
             var tolerance = new TextToleranceFactory().Instantiate("hamming(0.8)");
             Assert.That(tolerance, Is.TypeOf<TextTolerance>());
             Assert.That(tolerance.Style, Is.EqualTo("Hamming distance"));
-            Assert.That(tolerance.Value, Is.EqualTo(0.8));
+            Assert.That(tolerance.Value, Is.EqualTo(0.8).Within(0.001));
             Assert.That(tolerance.Implementation, Is.Not.Null);
             Assert.That(tolerance.Implementation("alpha", "alpha"), Is.EqualTo(0));
         }

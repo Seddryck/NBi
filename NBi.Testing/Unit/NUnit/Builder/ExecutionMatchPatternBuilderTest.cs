24a70ec23769e4b7881dc9c0493153f4d4b51d89
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-10-28 17:38:39 +0200

    An additional fix

--- NBi.Testing/Unit/NUnit/Builder/ExecutionMatchPatternBuilderTest.cs
+++ NBi.Testing/Unit/NUnit/Builder/ExecutionMatchPatternBuilderTest.cs
@@ -8,6 +8,7 @@ using NBi.Xml.Constraints;
 using NBi.Xml.Items;
 using NBi.Xml.Systems;
 using NUnit.Framework;
+using NBi.Core.ResultSet;
 #endregion
 
 namespace NBi.Testing.Unit.NUnit.Builder
@@ -84,7 +85,7 @@ namespace NBi.Testing.Unit.NUnit.Builder
             builder.Build();
             var sut = builder.GetSystemUnderTest();
 
-            Assert.That(sut, Is.InstanceOf<IDbCommand>());
+            Assert.That(sut, Is.InstanceOf<IResultSetService>());
         }
 
     }

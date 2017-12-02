62cd282b00a2d1d73da91cb9d91eba297f96a8fe
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-12-02 10:51:28 +0100

    Fix issue with seralization of an empty tag for variables when the collection is empyt

---
+++ NBi.Testing/Unit/Xml/Variables/GlobalVariableXmlTest.cs
@@ -0,0 +1,135 @@
+﻿using NBi.Xml;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBi.Xml.Variables;
+using System.Xml.Serialization;
+using System.IO;
+using System.Diagnostics;
+using NBi.Xml.Items;
+
+namespace NBi.Testing.Unit.Xml.Variables
+{
+    public class GlobalVariableXmlTest
+    {
+        [Test]
+        public void Serialize_OneCSharp_Correct()
+        {
+            var testSuiteXml = new TestSuiteXml()
+            {
+                Variables = new List<GlobalVariableXml>()
+                {
+                    new GlobalVariableXml()
+                    {
+                        Name="myCSharp",
+                        Script = new ScriptXml() {Language=NBi.Core.Transformation.LanguageType.CSharp, Code="0+0" }
+                    }
+                }
+            };
+
+            var serializer = new XmlSerializer(typeof(TestSuiteXml));
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream, Encoding.UTF8);
+            serializer.Serialize(writer, testSuiteXml);
+            var content = Encoding.UTF8.GetString(stream.ToArray());
+            writer.Close();
+            stream.Close();
+
+            Debug.WriteLine(content);
+
+            Assert.That(content, Is.StringContaining("<variables"));
+            Assert.That(content, Is.StringContaining("<variable name=\"myCSharp\""));
+            Assert.That(content, Is.StringContaining("0+0"));
+        }
+
+        [Test]
+        public void Serialize_OneQuery_Correct()
+        {
+            var testSuiteXml = new TestSuiteXml()
+            {
+                Variables = new List<GlobalVariableXml>()
+                {
+                    new GlobalVariableXml()
+                    {
+                        Name="mySQL",
+                        QueryScalar = new QueryScalarXml() {ConnectionString= "myConnString", InlineQuery="select * from myTable;"}
+                    }
+                }
+            };
+
+            var serializer = new XmlSerializer(typeof(TestSuiteXml));
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream, Encoding.UTF8);
+            serializer.Serialize(writer, testSuiteXml);
+            var content = Encoding.UTF8.GetString(stream.ToArray());
+            writer.Close();
+            stream.Close();
+
+            Debug.WriteLine(content);
+
+            Assert.That(content, Is.StringContaining("<variables"));
+            Assert.That(content, Is.StringContaining("<variable name=\"mySQL\""));
+            Assert.That(content, Is.StringContaining("select * from myTable;"));
+        }
+
+        [Test]
+        public void Serialize_TwoVariables_Correct()
+        {
+            var testSuiteXml = new TestSuiteXml()
+            {
+                Variables = new List<GlobalVariableXml>()
+                {
+                    new GlobalVariableXml()
+                    {
+                        Name="mySQL",
+                        QueryScalar = new QueryScalarXml() {ConnectionString= "myConnString", InlineQuery="select * from myTable;"}
+                    },
+                    new GlobalVariableXml()
+                    {
+                        Name="myCSharp",
+                        Script = new ScriptXml() {Language=NBi.Core.Transformation.LanguageType.CSharp, Code="0+0" }
+                    }
+                }
+            };
+
+            var serializer = new XmlSerializer(typeof(TestSuiteXml));
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream, Encoding.UTF8);
+            serializer.Serialize(writer, testSuiteXml);
+            var content = Encoding.UTF8.GetString(stream.ToArray());
+            writer.Close();
+            stream.Close();
+
+            Debug.WriteLine(content);
+
+            Assert.That(content, Is.StringContaining("<variables"));
+            Assert.That(content, Is.StringContaining("<variable name=\"mySQL\""));
+            Assert.That(content, Is.StringContaining("<variable name=\"myCSharp\""));
+        }
+
+        [Test]
+        public void Serialize_NoVariable_NothingSerialized()
+        {
+            var testSuiteXml = new TestSuiteXml()
+            {
+                Variables = new List<GlobalVariableXml>()
+            };
+
+            var serializer = new XmlSerializer(typeof(TestSuiteXml));
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream, Encoding.UTF8);
+            serializer.Serialize(writer, testSuiteXml);
+            var content = Encoding.UTF8.GetString(stream.ToArray());
+            writer.Close();
+            stream.Close();
+
+            Debug.WriteLine(content);
+
+            Assert.That(content, Is.Not.StringContaining("<variables"));
+            Assert.That(content, Is.Not.StringContaining("<variable"));
+        }
+    }
+}

d4afe37486cdce40e6779b5458daca900c8d8db3
Cédric L. Charlier <cedric.charlier@yucom.be>
2017-12-03 14:54:31 +0100

    Simplification of DbCommandFactory

--- NBi.Core/Query/DbCommandFactory.cs
+++ NBi.Core/Query/DbCommandFactory.cs
@@ -14,31 +14,21 @@ namespace NBi.Core.Query
             return Build(connection, query.Statement, query.CommandType, query.Parameters, query.TemplateTokens, Convert.ToInt32(query.Timeout.TotalSeconds));
         }
 
-        public IDbCommand Build(string connectionString, string query, IEnumerable<IQueryParameter> parameters)
+        internal IDbCommand Build(string connectionString, string query, IEnumerable<IQueryParameter> parameters)
         {
             var factory = new ConnectionFactory();
             var connection = factory.Instantiate(connectionString);
-            return Build(connection, query, parameters);
+            return Build(connection, query, CommandType.Text, parameters, null, 0);
         }
 
-        public IDbCommand Build(string connectionString, string query, IEnumerable<IQueryParameter> parameters, IEnumerable<IQueryTemplateVariable> variables, TimeSpan timeout)
+        internal IDbCommand Build(string connectionString, string query, IEnumerable<IQueryParameter> parameters, IEnumerable<IQueryTemplateVariable> variables, TimeSpan timeout)
         {
             var factory = new ConnectionFactory();
             var connection = factory.Instantiate(connectionString);
-            return Build(connection, query, parameters, variables, Convert.ToInt32(timeout.TotalSeconds));
+            return Build(connection, query, CommandType.Text, parameters, variables, Convert.ToInt32(timeout.TotalSeconds));
         }
 
-        public IDbCommand Build(IDbConnection connection, string query, IEnumerable<IQueryParameter> parameters)
-        {
-            return Build(connection, query, parameters, null);
-        }
-
-        public IDbCommand Build(IDbConnection connection, string query, IEnumerable<IQueryTemplateVariable> variables)
-        {
-            return Build(connection, query, null, variables);
-        }
-
-        public IDbCommand Build(IDbConnection connection, string query, IEnumerable<IQueryParameter> parameters, IEnumerable<IQueryTemplateVariable> variables)
+        protected IDbCommand Build(IDbConnection connection, string query, CommandType commandType, IEnumerable<IQueryParameter> parameters, IEnumerable<IQueryTemplateVariable> variables, int timeout)
         {
             var cmd = connection.CreateCommand();
 
@@ -73,24 +63,14 @@ namespace NBi.Core.Query
                 }
             }
 
-            return cmd;
-        }
-
-        public IDbCommand Build(IDbConnection connection, string text, CommandType commandType, IEnumerable<IQueryParameter> parameters, IEnumerable<IQueryTemplateVariable> variables, int timeout)
-        {
-            var cmd = Build(connection, text, parameters, variables, timeout);
             cmd.CommandType = commandType;
-            return cmd;
-        }
 
-        public IDbCommand Build(IDbConnection connection, string query, IEnumerable<IQueryParameter> parameters, IEnumerable<IQueryTemplateVariable> variables, int timeout)
-        {
-            var cmd = Build(connection, query, parameters, variables);
             var commandTimeout = Math.Max(0, timeout);
             cmd.CommandTimeout = commandTimeout;
+
             return cmd;
         }
-
+        
         private string ApplyVariablesToTemplate(string template, IEnumerable<IQueryTemplateVariable> variables)
         {
             var templateEngine = new StringTemplateEngine(template, variables);

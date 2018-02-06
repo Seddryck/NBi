876b638a2b0d3a817cbea8d7d346f08c37690bf9
Cédric L. Charlier <cedric.charlier@yucom.be>
2018-02-06 23:05:38 +0100

    Serialization of referential integrity tests

---
+++ NBi.Xml/Constraints/ReferenceExistsXml.cs
@@ -0,0 +1,39 @@
+﻿using NBi.Xml.Items.ResultSet;
+using NBi.Xml.Settings;
+using NBi.Xml.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NBi.Xml.Constraints
+{
+    public class ReferenceExistsXml : AbstractConstraintXml
+    {
+        [XmlElement("column-mapping")]
+        public List<ColumnMappingXml> Mappings { get; set; }
+
+        [XmlElement("resultSet")]
+        public ResultSetSystemXml ResultSet { get; set; }
+
+        public ReferenceExistsXml()
+        {
+            Mappings = new List<ColumnMappingXml>();
+        }
+
+        [XmlIgnore()]
+        public override DefaultXml Default
+        {
+            get { return base.Default; }
+            set
+            {
+                base.Default = value;
+                if (ResultSet != null)
+                    ResultSet.Default = value;
+            }
+        }
+
+    }
+}

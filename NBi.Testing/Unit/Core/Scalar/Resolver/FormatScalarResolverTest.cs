af0b60d7eff881582eb75db11bb8dec673a46bed
Cédric L. Charlier <cedric.charlier@yucom.be>
2018-11-10 15:21:04 +0100

    Implement and test formatting

---
+++ NBi.Testing/Unit/Core/Scalar/Resolver/FormatScalarResolverTest.cs
@@ -0,0 +1,41 @@
+﻿using NBi.Core.Injection;
+using NBi.Core.Scalar.Resolver;
+using NBi.Core.Variable;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Testing.Unit.Core.Scalar.Resolver
+{
+    public class FormatScalarResolverTest
+    {
+        [Test]
+        public void Execute_ExistingNumericVariable_CorrectEvaluation()
+        {
+            var globalVariables = new Dictionary<string, ITestVariable>()
+            {
+                { "myVar" , new TestVariable(new CSharpScalarResolver<object>( new CSharpScalarResolverArgs("10*10"))) },
+                { "otherVar" , new TestVariable(new CSharpScalarResolver<object>( new CSharpScalarResolverArgs("10+10"))) }
+            };
+            var args = new FormatScalarResolverArgs("Twenty = {@otherVar:#0.00}?", globalVariables);
+            var resolver = new FormatScalarResolver(args, new ServiceLocator());
+            Assert.That(resolver.Execute(), Is.EqualTo("Twenty = 20.00?"));
+        }
+
+        [Test]
+        [SetCulture("fr-fr")]
+        public void Execute_ExistingDateTimeVariable_CorrectEvaluation()
+        {
+            var globalVariables = new Dictionary<string, ITestVariable>()
+            {
+                { "myVar" , new TestVariable(new CSharpScalarResolver<object>( new CSharpScalarResolverArgs("new DateTime(2018,1,1)"))) },
+            };
+            var args = new FormatScalarResolverArgs("First day of 2018 is a {@myVar:dddd}", globalVariables);
+            var resolver = new FormatScalarResolver(args, new ServiceLocator());
+            Assert.That(resolver.Execute(), Is.EqualTo("First day of 2018 is a Monday"));
+        }
+    }
+}

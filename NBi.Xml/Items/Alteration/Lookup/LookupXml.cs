ccab6c240bbca21ca023813b4fb45ad9a1c4aa95
Cédric L. Charlier <cedric.charlier@yucom.be>
2018-12-31 14:23:20 +0100

    Serailize and desrialize the new lookup alteration

---
+++ NBi.Xml/Items/Alteration/Lookup/LookupXml.cs
@@ -0,0 +1,35 @@
+﻿using NBi.Xml.Items.ResultSet.Lookup;
+using NBi.Xml.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace NBi.Xml.Items.Alteration.Lookup
+{
+    public class LookupXml
+    {
+        [XmlElement("missing")]
+        public MissingXml Missing { get; set; }
+
+        [XmlIgnore()]
+        public bool MissingSpecified
+        {
+            get => Missing != MissingXml.Default;
+            set { }
+        }
+
+        [XmlElement("join")]
+        public JoinXml Join { get; set; }
+
+        [XmlElement("result-set")]
+        public ResultSetSystemXml ResultSet { get; set; }
+
+        public LookupXml()
+        {
+            Missing = MissingXml.Default;
+        }
+    }
+}

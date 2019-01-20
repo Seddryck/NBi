12c1a88708eb9ea4d1a36fa0a859ffa65df3b347
Cédric L. Charlier <cedric.charlier@yucom.be>
2019-01-20 18:27:55 +0100

    Fix a bug switching empty and missing cell when creating a csv-profile

--- NBi.Testing/Unit/Core/FlatFile/CsvReaderTest.cs
+++ NBi.Testing/Unit/Core/FlatFile/CsvReaderTest.cs
@@ -386,5 +386,45 @@ namespace NBi.Testing.Core.FlatFile
                 }
             }
         }
+
+        [Test]
+        public void Read_EmptyValue_MatchWithEmpty()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write("a;;c");
+                    writer.Flush();
+
+                    stream.Position = 0;
+
+                    var profile = CsvProfile.SemiColumnDoubleQuote;
+                    var reader = new CsvReaderProxy(profile);
+                    var dataTable = reader.Read(stream);
+                    Assert.That(dataTable.Rows[0][1], Is.EqualTo(string.Empty));
+                }
+            }
+        }
+
+        [Test]
+        public void Read_MissingValue_MatchWithNullValue()
+        {
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write("a;b;c\r\na;b\r\na;b;c");
+                    writer.Flush();
+
+                    stream.Position = 0;
+
+                    var profile = CsvProfile.SemiColumnDoubleQuote;
+                    var reader = new CsvReaderProxy(profile);
+                    var dataTable = reader.Read(stream);
+                    Assert.That(dataTable.Rows[1][2], Is.EqualTo("(null)"));
+                }
+            }
+        }
     }
 }

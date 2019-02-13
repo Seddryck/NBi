459e2b7d55173e627ad5320ffce4744de140b98c
Cédric L. Charlier <cedric.charlier@yucom.be>
2019-02-13 13:05:24 +0100

    Add support for combined variable and native transformation

---
+++ NBi.Core/Scalar/Resolver/FunctionScalarResolverArgs.cs
@@ -0,0 +1,18 @@
+﻿using NBi.Core.Transformation.Transformer.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.Scalar.Resolver
+{
+    public class FunctionScalarResolverArgs : IScalarResolverArgs
+    {
+        public IScalarResolver Resolver { get; }
+        public IEnumerable<INativeTransformation> Transformations { get; }
+
+        public FunctionScalarResolverArgs(IScalarResolver resolver, IEnumerable<INativeTransformation> transformations)
+            => (Resolver, Transformations) = (resolver, transformations);
+    }
+}

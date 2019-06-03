922700c1bfa77402f586614770499eb29f89f6cf
Cédric L. Charlier <cedric.charlier@yucom.be>
2019-06-04 00:39:42 +0200

    Add the few native transformations related to a path

---
+++ NBi.Core/Transformation/Transformer/Native/IO/PathTransformation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NBi.Core.Transformation.Transformer.Native.IO
+{
+    abstract class AbstractPathTransformation : AbstractTextTransformation
+    {
+        protected override object EvaluateNull() => "(empty)";
+        protected override object EvaluateEmpty() => "(empty)";
+        protected override object EvaluateBlank() => "(empty)";
+        protected override object EvaluateSpecial(string value) => "(empty)";
+    }
+
+    class PathToFilename : AbstractPathTransformation
+    {
+        protected override object EvaluateString(string value) => Path.GetFileName(value);
+    }
+
+    class PathToFilenameWithoutExtension : AbstractPathTransformation
+    {
+        protected override object EvaluateString(string value) => Path.GetFileNameWithoutExtension(value);
+    }
+
+    class PathToExtension : AbstractPathTransformation
+    {
+        protected override object EvaluateString(string value) => Path.GetExtension(value);
+    }
+
+    class PathToRoot : AbstractPathTransformation
+    {
+        protected override object EvaluateString(string value) => Path.GetPathRoot(value);
+    }
+
+    class PathToDirectory : AbstractPathTransformation
+    {
+        protected override object EvaluateString(string value)
+            => Path.GetDirectoryName(value)==null ? Path.GetPathRoot(value) : Path.GetDirectoryName(value) + Path.DirectorySeparatorChar;
+    }
+}
---
+++ NBi.Testing/Unit/Core/Transformation/Transformer/Native/PathTest.cs
@@ -0,0 +1,80 @@
+﻿using NBi.Core.Transformation.Transformer.Native;
+using NBi.Core.Transformation.Transformer.Native.IO;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Testing.Unit.Core.Transformation.Transformer.Native
+{
+    [TestFixture]
+    public class PathTest
+    {
+        [Test]
+        [TestCase(@"C:\", "")]
+        [TestCase(@"C:\Dir\", "")]
+        [TestCase(@"C:\Dir\Child\", "")]
+        [TestCase(@"C:\Dir\ChildFile", "ChildFile")]
+        [TestCase(@"C:\Dir\Child\file.txt", "file.txt")]
+        public void Execute_PathToFilename_Valid(string value, string expected)
+        {
+            var function = new PathToFilename();
+            var result = function.Evaluate(value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(@"C:\", "")]
+        [TestCase(@"C:\Dir\", "")]
+        [TestCase(@"C:\Dir\Child\", "")]
+        [TestCase(@"C:\Dir\ChildFile", "ChildFile")]
+        [TestCase(@"C:\Dir\Child\file.txt", "file")]
+        public void Execute_PathToFilenameWithoutExtension_Valid(string value, string expected)
+        {
+            var function = new PathToFilenameWithoutExtension();
+            var result = function.Evaluate(value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(@"C:\", "")]
+        [TestCase(@"C:\Dir\", "")]
+        [TestCase(@"C:\Dir\Child\", "")]
+        [TestCase(@"C:\Dir\ChildFile", "")]
+        [TestCase(@"C:\Dir\Child\file.txt", ".txt")]
+        public void Execute_PathToExtension_Valid(string value, string expected)
+        {
+            var function = new PathToExtension();
+            var result = function.Evaluate(value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(@"C:\")]
+        [TestCase(@"C:\Dir\")]
+        [TestCase(@"C:\Dir\Child\")]
+        [TestCase(@"C:\Dir\ChildFile")]
+        [TestCase(@"C:\Dir\Child\file.txt")]
+        public void Execute_PathToRoot_Valid(string value)
+        {
+            var function = new PathToRoot();
+            var result = function.Evaluate(value);
+            Assert.That(result, Is.EqualTo(@"C:\"));
+        }
+
+        [Test]
+        [TestCase(@"C:\", @"C:\")]
+        [TestCase(@"C:\Dir\", @"C:\Dir\")]
+        [TestCase(@"C:\Dir\Child\", @"C:\Dir\Child\")]
+        [TestCase(@"C:\Dir\ChildFile", @"C:\Dir\")]
+        [TestCase(@"C:\Dir\Child\file.txt", @"C:\Dir\Child\")]
+        public void Execute_PathToDirectory_Valid(string value, string expected)
+        {
+            var function = new PathToDirectory();
+            var result = function.Evaluate(value);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}

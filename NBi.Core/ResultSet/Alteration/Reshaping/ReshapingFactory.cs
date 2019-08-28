06bc3717abb00e5f5a58ee2c740ea21cc7d1c131
Cédric L. Charlier <cedric.charlier@yucom.be>
2019-08-28 08:15:59 +0200

    Implementation of the Unstack engine

---
+++ NBi.Core/ResultSet/Alteration/Reshaping/ReshapingFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.ResultSet.Alteration.Reshaping
+{
+    public class ReshapingFactory
+    {
+        public IReshapingEngine Instantiate(IReshapingArgs args)
+        {
+            switch(args)
+            {
+                case UnstackArgs x: return new UnstackEngine(x);
+                default: throw new ArgumentException();
+            }
+        }
+    }
+}

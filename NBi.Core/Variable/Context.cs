f195481948e81140882c3aa36a6aa28d8cc1a7cd
Cédric L. Charlier <cedric.charlier@yucom.be>
2019-10-06 22:24:44 +0200

    Add the new ScalarResolver for context

---
+++ NBi.Core/Variable/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.Variable
+{
+    public class Context
+    {
+        public DataRow CurrentRow { get; private set; }
+
+        public void Switch(DataRow currentRow)
+            => CurrentRow = currentRow;
+    }
+}

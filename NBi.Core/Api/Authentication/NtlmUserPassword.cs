51e1937f5ee50ac560e6326cb5c3b5a5d5ec7934
Cédric L. Charlier <cedric.charlier@yucom.be>
2019-10-29 21:14:40 +0100

    Implement the RestEngine and its authorizations

---
+++ NBi.Core/Api/Authentication/NtlmUserPassword.cs
@@ -0,0 +1,22 @@
+﻿using NBi.Core.Scalar.Resolver;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBi.Core.Api.Authentication
+{
+    class NtlmUserPassword : IAuthentication
+    {
+        public IScalarResolver<string> Username { get; }
+        public IScalarResolver<string> Password { get; }
+
+        public NtlmUserPassword(IScalarResolver<string> username, IScalarResolver<string> password)
+            => (Username, Password) = (username, password);
+
+        public IAuthenticator GetAuthenticator() => new NtlmAuthenticator(Username.Execute(), Password.Execute());
+    }
+
+}

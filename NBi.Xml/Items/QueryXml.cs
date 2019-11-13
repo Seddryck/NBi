c92b8fb2647738f004c54e5be6b732d538c31048
Cédric L. Charlier <cedric.charlier@yucom.be>
2019-11-14 00:32:25 +0100

    Fix bug

--- NBi.Xml/Items/QueryXml.cs
+++ NBi.Xml/Items/QueryXml.cs
@@ -8,15 +8,31 @@ using System.Xml.Serialization;
 using NBi.Core;
 using NBi.Core.Query.Client;
 using NBi.Xml.SerializationOption;
+using NBi.Xml.Settings;
 
 namespace NBi.Xml.Items
 {
     public class QueryXml : QueryableXml
     {
-        
+        [XmlIgnore()]
+        private SettingsXml settings;
+
+        [XmlIgnore()]
+        public override SettingsXml Settings
+        {
+            get => settings;
+            set
+            {
+                settings = value;
+                if (Assembly != null)
+                    Assembly.Settings = value;
+            }
+        }
+
+
         [XmlAttribute("file")]
         public string File { get; set; }
-        
+
         [XmlAttribute("connectionString-ref")]
         public string ConnectionStringReference { get; set; }
 

dc8a9e114b2b324f1d9ee2b98f6d91c1602809a6
Cédric L. Charlier <cedric.charlier@yucom.be>
2020-01-05 19:06:31 +0100

    Implement and test the two new conditions

--- NBi.Core/Decoration/IO/Conditions/FileExistsCondition.cs
+++ NBi.Core/Decoration/IO/Conditions/FileExistsCondition.cs
@@ -7,24 +7,23 @@ using NBi.Core.Decoration.IO;
 using System.Diagnostics;
 using NBi.Extensibility;
 
-namespace NBi.Core.Decoration.IO.Commands
+namespace NBi.Core.Decoration.IO.Conditions
 {
-    class FolderExistsCondition : IDecorationCondition
+    class FileExistsCondition : IDecorationCondition
     {
-        private FolderExistsConditionArgs Args { get; }
+        private FileExistsConditionArgs Args { get; }
 
         public string Message => throw new NotImplementedException();
 
-        public FolderExistsCondition(FolderExistsConditionArgs args) => Args = args;
+        public FileExistsCondition(FileExistsConditionArgs args) => Args = args;
 
         public bool Validate()
         {
-            var path = PathExtensions.CombineOrRoot(Args.BasePath, Args.FolderPath.Execute());
-            var fullPath = PathExtensions.CombineOrRoot(path, Args.FolderName.Execute());
+            var fullPath = PathExtensions.CombineOrRoot(Args.BasePath, Args.FolderName.Execute(), Args.FileName.Execute());
 
             var conditions = new List<Func<string, bool>>() { ExistsCondition };
             if (Args.NotEmpty.Execute())
-                conditions.Add(IsEmptyCondition);
+                conditions.Add(IsNotEmptyCondition);
 
             var result = true;
             var enumerator = conditions.GetEnumerator();
@@ -34,9 +33,9 @@ namespace NBi.Core.Decoration.IO.Commands
         }
 
         protected bool ExistsCondition(string fullPath)
-            => Directory.Exists(fullPath);
+            => File.Exists(fullPath);
 
-        protected bool IsEmptyCondition(string fullPath)
-            => !string.IsNullOrEmpty(Directory.EnumerateFiles(fullPath).FirstOrDefault());
+        protected bool IsNotEmptyCondition(string fullPath)
+            => new FileInfo(fullPath).Length > 0;
     }
 }
---
+++ NBi.Core/Decoration/IO/Conditions/FolderExistsCondition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NBi.Core.Decoration.IO;
+using System.Diagnostics;
+using NBi.Extensibility;
+
+namespace NBi.Core.Decoration.IO.Conditions
+{
+    class FolderExistsCondition : IDecorationCondition
+    {
+        private FolderExistsConditionArgs Args { get; }
+
+        public string Message => throw new NotImplementedException();
+
+        public FolderExistsCondition(FolderExistsConditionArgs args) => Args = args;
+
+        public bool Validate()
+        {
+            var path = PathExtensions.CombineOrRoot(Args.BasePath, Args.FolderPath.Execute());
+            var fullPath = PathExtensions.CombineOrRoot(path, Args.FolderName.Execute());
+
+            var conditions = new List<Func<string, bool>>() { ExistsCondition };
+            if (Args.NotEmpty.Execute())
+                conditions.Add(IsNotEmptyCondition);
+
+            var result = true;
+            var enumerator = conditions.GetEnumerator();
+            while (result && enumerator.MoveNext())
+                result = enumerator.Current.Invoke(fullPath);
+            return result;
+        }
+
+        protected bool ExistsCondition(string fullPath)
+            => Directory.Exists(fullPath);
+
+        protected bool IsNotEmptyCondition(string fullPath)
+            => !string.IsNullOrEmpty(Directory.EnumerateFiles(fullPath).FirstOrDefault());
+    }
+}

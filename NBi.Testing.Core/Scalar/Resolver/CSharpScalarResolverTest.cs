8f5111ea9f2c3b47fa9f3b61f98470f42f4dea76
Lukzas <lukzas@gmail.com>
2020-03-01 18:45:29 +0100

    Added Xml related tests to CSharpScalarResolver suite

--- NBi.Testing.Core/Scalar/Resolver/CSharpScalarResolverTest.cs
+++ NBi.Testing.Core/Scalar/Resolver/CSharpScalarResolverTest.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace NBi.Testing.Core.Scalar.Resolver
 {
@@ -33,5 +35,33 @@ namespace NBi.Testing.Core.Scalar.Resolver
             Assert.That(output, Is.EqualTo(DateTime.Now.Year));
         }
 
+        [Test]
+        public void Instantiate_GetValueXmlLinq_CorrectComputation()
+        {
+            var xmlPath = new Uri(FileOnDisk.CreatePhysicalFile("PurchaseOrders.xml", "NBi.Testing.Core.Resources.PurchaseOrders.xml")).AbsolutePath;
+            string xmlDoc = string.Format(@"XDocument.Load(""{0}"").Root.Name.ToString()", xmlPath);
+
+            var args = new CSharpScalarResolverArgs(xmlDoc);
+            var resolver = new CSharpScalarResolver<string>(args);
+
+            var output = resolver.Execute();
+
+            Assert.That(output, Is.EqualTo(XDocument.Load(xmlPath).Root.Name.ToString()));
+        }
+
+        [Test]
+        public void Instantiate_GetValueXmlXpath_CorrectComputation()
+        {
+            var xPath = "./PurchaseOrders/PurchaseOrder/Address/Name";
+            var xmlPath = new Uri(FileOnDisk.CreatePhysicalFile("PurchaseOrders.xml", "NBi.Testing.Core.Resources.PurchaseOrders.xml")).AbsolutePath;
+            string xmlDoc = string.Format(@"XDocument.Load(""{0}"").XPathSelectElement(""{1}"").Value.ToString()", xmlPath, xPath);
+
+            var args = new CSharpScalarResolverArgs(xmlDoc);
+            var resolver = new CSharpScalarResolver<string>(args);
+
+            var output = resolver.Execute();
+
+            Assert.That(output, Is.EqualTo(XDocument.Load(xmlPath).XPathSelectElement(xPath).Value.ToString()));
+        }
     }
 }

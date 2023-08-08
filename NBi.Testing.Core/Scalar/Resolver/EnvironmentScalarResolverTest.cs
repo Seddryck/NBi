f18d7cb0840723770815225c25929f02d734979f
Felix Möller <mail@felixmoeller.de>
2023-08-09 00:45:51 +0200

    Update EnvironmentScalarResolverTest.cs

--- NBi.Testing.Core/Scalar/Resolver/EnvironmentScalarResolverTest.cs
+++ NBi.Testing.Core/Scalar/Resolver/EnvironmentScalarResolverTest.cs
@@ -14,10 +14,21 @@ namespace NBi.Testing.Core.Scalar.Resolver
         [SetUp]
         public void CreateEnvironmentVariable() => Environment.SetEnvironmentVariable("NBiTesting", "the_value");
         [TearDown]
-        public void DeleteEnvironmentVariable() => Environment.SetEnvironmentVariable("NBiTesting", null);
+        public void DeleteEnvironmentVariable() => Environment.SetEnvironmentVariable("NBiTestingUser", null, EnvironmentVariableTarget.User);
 
         [Test]
-        public void Instantiate_GetValueObject_CorrectRead()
+        public void Instantiate_GetValueObject_CorrectReadUser()
+        {
+            var args = new EnvironmentScalarResolverArgs("NBiTestingUser");
+            var resolver = new EnvironmentScalarResolver<string>(args);
+
+            var output = resolver.Execute();
+
+            Assert.That(output, Is.EqualTo("the_valueUser"));
+        }
+
+        [Test]
+        public void Instantiate_GetValueObject_CorrectReadProcess()
         {
             var args = new EnvironmentScalarResolverArgs("NBiTesting");
             var resolver = new EnvironmentScalarResolver<string>(args);
